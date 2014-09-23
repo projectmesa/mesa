581cc25aec320abf791fa941b7bdf74b62193322
David Masad <david.masad@gmail.com>
2014-09-22 22:27:57 -0400

    Adding Model, Agent and Grid
    Implementing placeholders for the Model and Agent objects, and a basic
    Grid object.

--- mesa/__init__.py
+++ mesa/__init__.py
@@ -4,7 +4,7 @@
 
 #From schelling
 #model.grid.get_neighbors(self.x, self.y, moore=True)
-
+'''
 empty
 it may or may not have grid
 it may have a scheduler; you can probably assume this
@@ -12,9 +12,48 @@ it may have a scheduler; you can probably assume this
 Scheduler maintains a list of agents
 To kill and agent, you remove it from the scheduler
 Then it won't exist. (You have to explicitly)
+'''
+
+import datetime as dt
+import random
+
+class Model(object):
+    '''
+    Base class for models.
+    '''
+    seed = None # Seed for the random number generator
+    schedule = None # Schedule object
+    running = None
+
+    def __init__(self, seed=None):
+        '''
+        Create a new model. Overload this method with the actual code to start 
+        the model.
+        '''
+        if seed is None:
+            self.seed = dt.now()
+        else:
+            self.seed = seed
+        random.seed(seed)
+        self.running = True
 
 
+    def run_model(self):
+        '''
+        Run the model until the end condition is reached. Overload as needed.
+        '''
+        while self.running:
+            self.step()
 
+
+    def step(self):
+        '''
+        A single step. Fill in here.
+        '''
+        pass
+
+
+'''
 # Agent
 # What do agents have
 
@@ -28,6 +67,30 @@ step()
 
 __init__()
     set the unique id
+'''
+
+class Agent(object):
+    '''
+    Base class for a model agent.
+    '''
+
+    model = None
+    unique_id = None
+
+    def __init__(self, unique_id, model):
+        '''
+        Create a new agent.
+        '''
+        self.model = model
+
+    def step(self, model):
+        '''
+        A single step of the agent.
+        '''
+        pass
+
+
+
 
 
 
--- mesa/space.py
+++ mesa/space.py
@@ -4,3 +4,85 @@
 
 #From Shell
 #self.grid = AgentGrid(height, width)
+
+
+class Grid(object):
+	'''
+	Base class for a square grid.
+	'''
+
+	width = None
+	height = None
+	torus = False
+	grid = None
+
+	def __init__(self, height, width, torus):
+		'''
+		Create a new grid.
+
+		Args:
+			height, width: The height and width of the grid
+			torus: Boolean whether the grid wraps or not.
+		'''
+		self.height = height
+		self.width = width
+		self.torus = torus
+
+		self.grid = []
+		for y in range(self.height):
+			row = []
+			for x in range(self.width):
+				row.append(None)
+			self.grid.append(row)
+
+	def __getitem__(self, index):
+		return self.grid[index]
+
+	def _get_x(self, x):
+		'''
+		Convert X coordinate, handling torus looping.
+		'''
+		if x >= 0 and x < self.width:
+			return x
+		if not self.torus:
+			raise Exception("Coordinate out of bounds.")
+		else:
+			return x % self.width
+
+	def _get_y(self, y):
+		'''
+		Convert Y coordinate, handling torus looping.
+		'''
+		if y >= 0 and y < self.height:
+			return y
+		if not self.torus:
+			raise Exception("Coordinate out of bounds.")
+		else:
+			return y % self.height
+
+	def get_neighbors(self, x, y, moore, include_center=False):
+		'''
+		Return a list of neighbors to a certain point.
+
+		Args:
+			x, y: Coordinates for the neighborhood to get.
+			moore: If True, return Moore neighborhood (including diagonals)
+				   If False, return Von Neumann neighborhood (exclude diagonals)
+			include_center: If True, return the (x, y) cell as well. Otherwise,
+							return surrounding cells only.
+		'''
+		neighbors = []
+		for dy in [-1, 0, 1]:
+			for dx in [-1, 0, 1]:
+				if dx == 0 and dy == 0 and not include_center: continue
+				if not moore and dy != 0 and dx != 0: continue
+				px = self._get_x(x + dx)
+				py = self._get_y(y + dy)
+				if self.grid[py][px] is not None:
+					neighbors.append(self.grid[py][px])
+		return neighbors
+
+
+
+
+
