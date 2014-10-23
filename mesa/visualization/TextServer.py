ecf6aa97c7d474bb8a48b98ce8214c4440e416fa
David Masad <david.masad@gmail.com>
2014-10-23 09:46:41 -0400

    Cleaned up the ascii server
    Added a real websocket protocol, fixed the ugly template path hack, and
    implemented resetting.

--- mesa/visualization/TextServer.py
+++ mesa/visualization/TextServer.py
@@ -1,6 +1,6 @@
 '''
 TextServer
-==================================================
+============================================================================
 
 A simple experimental Tornado server which renders an arbitrary ASCII 
 visualization in the browser.
@@ -8,27 +8,85 @@ visualization in the browser.
 Consists of the following classes:
 
 PageHandler: The handler for the visualization page, generated from a template.
-SocketHandler: Handles the websocket connection between 
+SocketHandler: Handles the websocket connection between the client page and 
+                the server.
+TextServer: The overall controller class which stores and controls 
+            the model and visualization instance.
 
+In theory, TextServer does NOT need to be subclassed on a model-by-model basis;
+it should be able to create a browser visualization for any given standard.
+TextVisualization subclass. 
 
+A TextServer is created using two classes: one for the model, and one for the 
+associated TextVisualization child class. The new TextServer can also accept 
+arguments to pass to the first model instance. To go with the perennial Schelling
+example, the following code 
+
+    server = TextServer(SchellingModel, SchellingTextVisualization, "Schelling",
+                            10, 10, 0.8, 0.2, 3)
+
+creates a TextServer visualizing a SchellingModel via the 
+SchellingTextVisualization, named "Schelling", with 10, 10... , 3 as the default
+model parameters.
+
+The server is launched using the launch() method. This runs the Tornado app, 
+which consists of PageHandler and SocketHandler. At the moment, the server runs
+the entire model (up to a default max_steps) prior to actually serving the data.
+TODO: Make the model run a coroutine.
+A dictionary associating each TextVisualization element's positional index and
+visualization text is stored in a the TextServer's viz_state list, with position 
+corresponding to the step it represents.
+
+The client keeps track of what step it is showing. Clicking the Step button in
+the browser sends a message requesting the viz_state corresponding to the next
+step position, which is then sent back to the client via the websocket.
+
+The websocket protocol is as follows: 
+Each message is a JSON object, with a "type" property which defines the rest of
+the structure.
+
+Server -> Client
+
+    {
+    "type": "viz_state",
+    "data": A JSON representation of the viz state text
+    }
+
+Client -> Server
+    
+    Reset the model. 
+    TODO: Allow this to come with parameters
+    {
+    "type": "reset"
+    }
+
+    Get a given state.
+    {
+    "type": "get_step",
+    "step:" index of the step to get. 
+    }
 '''
+
+import os
 
 import tornado.ioloop
 import tornado.template
 import tornado.web
 import tornado.websocket
-
+import tornado.escape
 
 class PageHandler(tornado.web.RequestHandler):
     def initialize(self, controller):
         self.controller = controller
         # TODO: This is a horrible hack and there's got to be a better way.
-        path = "/Users/dmasad/Programming/ProjectMesa/mesa/mesa/visualization/templates"
+        path = os.path.dirname(__file__) + "/templates"
         loader = tornado.template.Loader(path)
         self.template = loader.load("text_template.html")
 
     def get(self):
-        page = self.template.generate(model_name=self.controller.model_name,
+        page = self.template.generate(
+                    port = self.controller.port,
+                    model_name=self.controller.model_name,
                     element_count = len(self.controller.visualization.elements))
         self.write(page)
 
@@ -38,22 +96,34 @@ class SocketHandler(tornado.websocket.WebSocketHandler):
         self.controller = controller
 
     def open(self):
-        print("Socket opened!")
+        if self.controller.verbose:
+            print("Socket opened!")
 
     def on_message(self, message):
         '''
-        Receiving a message from the websocket.
+        Receiving a message from the websocket, parse, and act accordingly.
         '''
-        print(message)
-        if "get_step:" in message:
-            step = int(message.split(":")[1])
-            self.write_message(self.controller.viz_states[step])
-        elif message == "RESET":
+        if self.controller.verbose:
+            print(message)
+        msg = tornado.escape.json_decode(message)
+
+        if msg["type"] == "get_step":
+            step = int(msg["step"])
+            return_message = {"type": "viz_state"}
+            return_message["data"] = self.controller.viz_states[step]
+            self.write_message(return_message)
+
+        elif msg["type"] == "reset":
             self.controller.reset()
-            self.write_message(self.controller.viz_states[0])
+            return_message = {"type": "viz_state"}
+            return_message["data"] = self.controller.viz_states[0]
+
+            self.write_message(return_message)
             self.controller.run_model()
+
         else:
-            print("Unexpected message!")
+            if self.controller.verbose:
+                print("Unexpected message!")
 
 
 
@@ -65,6 +135,8 @@ class TextServer(object):
 
     '''
 
+    verbose = True
+
     model_name = "Mesa Model"
     model_cls = None # A model class
     text_visualization_cls = None # A visualization associated with that model
@@ -72,6 +144,9 @@ class TextServer(object):
 
     max_steps = 100
     viz_states = []
+
+    model_args = ()
+    model_kwargs = {}
 
     def __init__(self, model_cls, text_visualization_cls, name="Mesa Model",
                     *args, **kwargs):
@@ -89,7 +164,11 @@ class TextServer(object):
         self.model_cls = model_cls
         self.text_visualization_cls = text_visualization_cls
 
-        self.reset(*args, **kwargs)
+        # TODO: Make this more elegant
+        self.model_args = args
+        self.model_kwargs = kwargs
+
+        self.reset()
 
         self.application = tornado.web.Application([
             (r'/', PageHandler, {"controller": self}),
@@ -100,13 +179,14 @@ class TextServer(object):
         '''
         Launch the server and have it listen on the given port.
         '''
+        self.port = port
         print("Launching server, listening on port", port)
         self.run_model()
         self.application.listen(port)
         tornado.ioloop.IOLoop.instance().start()
 
-    def reset(self, *args, **kwargs):
-        self.model = self.model_cls(*args, **kwargs)
+    def reset(self):
+        self.model = self.model_cls(*self.model_args, **self.model_kwargs)
         self.visualization = self.text_visualization_cls(self.model)
         self.viz_states = [self.get_viz()]
 
