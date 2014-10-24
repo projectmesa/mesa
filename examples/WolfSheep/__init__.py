b13748ecf68bcc7584a98d47d438bde7a3b4b710
David Masad <david.masad@gmail.com>
2014-10-24 15:22:39 -0400

    Adding in WolfSheepPredation

---
+++ examples/WolfSheep/__init__.py
@@ -0,0 +1,6 @@
+from WolfSheep import WolfSheepPredation
+from WolfSheepVisualization import WolfSheepVisualization
+
+if __name__ == "__main__":
+	model = WolfSheepPredation()
+	viz = WolfSheepVisualization(model)
