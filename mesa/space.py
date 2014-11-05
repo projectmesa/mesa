d14d0a0604d56dc7755665eeed27f9a347037f20
David Masad <david.masad@gmail.com>
2014-11-05 12:05:03 -0500

    Changing tabs to spaces
    Well this is embarrassing...

--- mesa/space.py
+++ mesa/space.py
@@ -2,182 +2,182 @@
 Mesa Space Module
 =================================
 
-Objects used to add a spatial component to a model. 
+Objects used to add a spatial component to a model.
 
-Grid: base grid, a simple list-of-lists. 
+Grid: base grid, a simple list-of-lists.
 
 MultiGrid: extension to Grid where each cell is a set of objects.
 
 '''
 
 class Grid(object):
-	'''
-	Base class for a square grid.
+    '''
+    Base class for a square grid.
 
-	Grid cells are indexed by [y][x], where [0][0] is assumed to be the top-left
-	and [height-1][width-1] is the bottom-right. If a grid is toroidal, the top
-	and bottom, and left and right, edges wrap to each other
+    Grid cells are indexed by [y][x], where [0][0] is assumed to be the top-left
+    and [height-1][width-1] is the bottom-right. If a grid is toroidal, the top
+    and bottom, and left and right, edges wrap to each other
 
-	Properties:
-		width, height: The grid's width and height. 
-		torus: Boolean which determines whether to treat the grid as a torus.
+    Properties:
+        width, height: The grid's width and height. 
+        torus: Boolean which determines whether to treat the grid as a torus.
 
-		grid: Internal list-of-lists which holds the grid cells themselves.
+        grid: Internal list-of-lists which holds the grid cells themselves.
 
-	Methods:
-		get_neighbors: Returns the objects surrounding a given cell.
-	'''
+    Methods:
+        get_neighbors: Returns the objects surrounding a given cell.
+    '''
 
-	width = None
-	height = None
-	torus = False
-	grid = None
+    width = None
+    height = None
+    torus = False
+    grid = None
 
-	def __init__(self, height, width, torus):
-		'''
-		Create a new grid.
+    def __init__(self, height, width, torus):
+        '''
+        Create a new grid.
 
-		Args:
-			height, width: The height and width of the grid
-			torus: Boolean whether the grid wraps or not.
-		'''
-		self.height = height
-		self.width = width
-		self.torus = torus
+        Args:
+            height, width: The height and width of the grid
+            torus: Boolean whether the grid wraps or not.
+        '''
+        self.height = height
+        self.width = width
+        self.torus = torus
 
-		self.grid = []
-		for y in range(self.height):
-			row = []
-			for x in range(self.width):
-				row.append(None)
-			self.grid.append(row)
+        self.grid = []
+        for y in range(self.height):
+            row = []
+            for x in range(self.width):
+                row.append(None)
+            self.grid.append(row)
 
-	def __getitem__(self, index):
-		return self.grid[index]
+    def __getitem__(self, index):
+        return self.grid[index]
 
-	def _get_x(self, x):
-		'''
-		Convert X coordinate, handling torus looping.
-		'''
-		if x >= 0 and x < self.width:
-			return x
-		if not self.torus:
-			raise Exception("Coordinate out of bounds.")
-		else:
-			return x % self.width
+    def _get_x(self, x):
+        '''
+        Convert X coordinate, handling torus looping.
+        '''
+        if x >= 0 and x < self.width:
+            return x
+        if not self.torus:
+            raise Exception("Coordinate out of bounds.")
+        else:
+            return x % self.width
 
-	def _get_y(self, y):
-		'''
-		Convert Y coordinate, handling torus looping.
-		'''
-		if y >= 0 and y < self.height:
-			return y
-		if not self.torus:
-			raise Exception("Coordinate out of bounds.")
-		else:
-			return y % self.height
+    def _get_y(self, y):
+        '''
+        Convert Y coordinate, handling torus looping.
+        '''
+        if y >= 0 and y < self.height:
+            return y
+        if not self.torus:
+            raise Exception("Coordinate out of bounds.")
+        else:
+            return y % self.height
 
-	def get_neighbors(self, x, y, moore, include_center=False):
-		'''
-		Return a list of neighbors to a certain point.
+    def get_neighbors(self, x, y, moore, include_center=False):
+        '''
+        Return a list of neighbors to a certain point.
 
-		Args:
-			x, y: Coordinates for the neighborhood to get.
-			moore: If True, return Moore neighborhood (including diagonals)
-				   If False, return Von Neumann neighborhood (exclude diagonals)
-			include_center: If True, return the (x, y) cell as well. Otherwise,
-							return surrounding cells only.
+        Args:
+            x, y: Coordinates for the neighborhood to get.
+            moore: If True, return Moore neighborhood (including diagonals)
+                   If False, return Von Neumann neighborhood (exclude diagonals)
+            include_center: If True, return the (x, y) cell as well. Otherwise,
+                            return surrounding cells only.
 
-		Returns:
-			A list of non-None objects in the given neighborhood; at most 9 if 
-			Moore, 5 if Von-Neumann (8 and 4 if not including the center).
-		'''
-		neighbors = []
-		for dy in [-1, 0, 1]:
-			for dx in [-1, 0, 1]:
-				if dx == 0 and dy == 0 and not include_center: continue
-				if not moore and dy != 0 and dx != 0: continue
-				px = self._get_x(x + dx)
-				py = self._get_y(y + dy)
-				if self.grid[py][px] is not None:
-					neighbors.append(self.grid[py][px])
-		return neighbors
+        Returns:
+            A list of non-None objects in the given neighborhood; at most 9 if 
+            Moore, 5 if Von-Neumann (8 and 4 if not including the center).
+        '''
+        neighbors = []
+        for dy in [-1, 0, 1]:
+            for dx in [-1, 0, 1]:
+                if dx == 0 and dy == 0 and not include_center: continue
+                if not moore and dy != 0 and dx != 0: continue
+                px = self._get_x(x + dx)
+                py = self._get_y(y + dy)
+                if self.grid[py][px] is not None:
+                    neighbors.append(self.grid[py][px])
+        return neighbors
 
 
 
 
 class MultiGrid(Grid):
-	'''
-	Grid where each cell can contain more than one object.
+    '''
+    Grid where each cell can contain more than one object.
 
-	Grid cells are indexed by [y][x], where [0][0] is assumed to be the top-left
-	and [height-1][width-1] is the bottom-right. If a grid is toroidal, the top
-	and bottom, and left and right, edges wrap to each other.
+    Grid cells are indexed by [y][x], where [0][0] is assumed to be the top-left
+    and [height-1][width-1] is the bottom-right. If a grid is toroidal, the top
+    and bottom, and left and right, edges wrap to each other.
 
-	Each grid cell holds a set object.
+    Each grid cell holds a set object.
 
-	Properties:
-		width, height: The grid's width and height. 
-		torus: Boolean which determines whether to treat the grid as a torus.
+    Properties:
+        width, height: The grid's width and height.
+        torus: Boolean which determines whether to treat the grid as a torus.
 
-		grid: Internal list-of-lists which holds the grid cells themselves.
+        grid: Internal list-of-lists which holds the grid cells themselves.
 
-	Methods:
-		get_neighbors: Returns the objects surrounding a given cell.
-	'''
+    Methods:
+        get_neighbors: Returns the objects surrounding a given cell.
+    '''
 
-	width = None
-	height = None
-	torus = False
-	grid = None
+    width = None
+    height = None
+    torus = False
+    grid = None
 
-	def __init__(self, height, width, torus):
-		'''
-		Create a new grid.
+    def __init__(self, height, width, torus):
+        '''
+        Create a new grid.
 
-		Args:
-			height, width: The height and width of the grid
-			torus: Boolean whether the grid wraps or not.
-		'''
-		self.height = height
-		self.width = width
-		self.torus = torus
+        Args:
+            height, width: The height and width of the grid
+            torus: Boolean whether the grid wraps or not.
+        '''
+        self.height = height
+        self.width = width
+        self.torus = torus
 
-		self.grid = []
-		for y in range(self.height):
-			row = []
-			for x in range(self.width):
-				row.append(set())
-			self.grid.append(row)
+        self.grid = []
+        for y in range(self.height):
+            row = []
+            for x in range(self.width):
+                row.append(set())
+            self.grid.append(row)
 
-	def __getitem__(self, index):
-		return self.grid[index]
+    def __getitem__(self, index):
+        return self.grid[index]
 
-	def get_neighbors(self, x, y, moore, include_center=False):
-		'''
-		Return a list of neighbors to a certain point.
+    def get_neighbors(self, x, y, moore, include_center=False):
+        '''
+        Return a list of neighbors to a certain point.
 
-		Args:
-			x, y: Coordinates for the neighborhood to get.
-			moore: If True, return Moore neighborhood (including diagonals)
-				   If False, return Von Neumann neighborhood (exclude diagonals)
-			include_center: If True, return the (x, y) cell as well. Otherwise,
-							return surrounding cells only.
+        Args:
+            x, y: Coordinates for the neighborhood to get.
+            moore: If True, return Moore neighborhood (including diagonals)
+                   If False, return Von Neumann neighborhood (exclude diagonals)
+            include_center: If True, return the (x, y) cell as well. Otherwise,
+                            return surrounding cells only.
 
-		Returns:
-			A list of non-None objects in the given neighborhood; at most 9 if 
-			Moore, 5 if Von-Neumann (8 and 4 if not including the center).
-		'''
-		neighbors = []
-		for dy in [-1, 0, 1]:
-			for dx in [-1, 0, 1]:
-				if dx == 0 and dy == 0 and not include_center: continue
-				if not moore and dy != 0 and dx != 0: continue
-				px = self._get_x(x + dx)
-				py = self._get_y(y + dy)
-				for a in self.grid[py][px]:
-					neighbors.append(a)
-		return neighbors
+        Returns:
+            A list of non-None objects in the given neighborhood; at most 9 if
+            Moore, 5 if Von-Neumann (8 and 4 if not including the center).
+        '''
+        neighbors = []
+        for dy in [-1, 0, 1]:
+            for dx in [-1, 0, 1]:
+                if dx == 0 and dy == 0 and not include_center: continue
+                if not moore and dy != 0 and dx != 0: continue
+                px = self._get_x(x + dx)
+                py = self._get_y(y + dy)
+                for a in self.grid[py][px]:
+                    neighbors.append(a)
+        return neighbors
 
 
 
--- mesa/time.py
+++ mesa/time.py
@@ -12,13 +12,13 @@ activating all agents in the same order every step, shuffling the activation
 order every time, activating each agent *on average* once per step, and more.
 
 Key concepts:
-	Step: Many models advance in 'steps'. A step may involve the activation of
-	all agents, or a random (or selected) subset of them. Each agent in turn 
-	may have their own step() method.
+    Step: Many models advance in 'steps'. A step may involve the activation of
+    all agents, or a random (or selected) subset of them. Each agent in turn 
+    may have their own step() method.
 
-	Time: Some models may simulate a continuous 'clock' instead of discrete
-	steps. However, by default, the Time is equal to the number of steps the
-	model has taken.
+    Time: Some models may simulate a continuous 'clock' instead of discrete
+    steps. However, by default, the Time is equal to the number of steps the
+    model has taken.
 
 
 TODO: Have the schedulers use the model's randomizer, to keep random number 
@@ -29,100 +29,87 @@ seeds consistent and allow for replication.
 import random
 
 class BaseScheduler(object):
-	'''
-	Simplest scheduler; activates agents one at a time, in the order they were
-	added. 
+    '''
+    Simplest scheduler; activates agents one at a time, in the order they were
+    added. 
 
-	Assumes that each agent added has a *step* method, which accepts a model
-	object as its single argument. 
+    Assumes that each agent added has a *step* method, which accepts a model
+    object as its single argument. 
 
-	(This is explicitly meant to replicate the scheduler in MASON).
-	'''
+    (This is explicitly meant to replicate the scheduler in MASON).
+    '''
 
-	model = None
-	steps = 0
-	time = 0
-	agents = []
+    model = None
+    steps = 0
+    time = 0
+    agents = []
 
-	def __init__(self, model):
-		'''
-		Create a new, empty BaseScheduler.
-		'''
+    def __init__(self, model):
+        '''
+        Create a new, empty BaseScheduler.
+        '''
 
-		self.model = model
-		self.steps = 0
-		self.time = 0
-		self.agents = []
+        self.model = model
+        self.steps = 0
+        self.time = 0
+        self.agents = []
 
-	def add(self, agent):
-		'''
-		Add an Agent object to the schedule.
+    def add(self, agent):
+        '''
+        Add an Agent object to the schedule.
 
-		Args:
-			agent: An Agent to be added to the schedule. NOTE: The agent must 
-			have a step(model) method.
-		'''
-		self.agents.append(agent)
+        Args:
+            agent: An Agent to be added to the schedule. NOTE: The agent must 
+            have a step(model) method.
+        '''
+        self.agents.append(agent)
 
-	def remove(self, agent):
-		'''
-		Remove all instances of a given agent from the schedule.
+    def remove(self, agent):
+        '''
+        Remove all instances of a given agent from the schedule.
 
-		Args:
-			agent: An agent object.
-		'''
-		while agent in self.agents:
-			self.agents.remove(agent)
+        Args:
+            agent: An agent object.
+        '''
+        while agent in self.agents:
+            self.agents.remove(agent)
 
 
-	def step(self):
-		'''
-		Execute the step of all the agents, one at a time.
-		'''
-		for agent in self.agents:
-			agent.step(self.model)
-		self.steps += 1
-		self.time += 1
+    def step(self):
+        '''
+        Execute the step of all the agents, one at a time.
+        '''
+        for agent in self.agents:
+            agent.step(self.model)
+        self.steps += 1
+        self.time += 1
 
-	def get_agent_count(self):
-		'''
-		Returns the current number of agents in the queue.
-		'''
+    def get_agent_count(self):
+        '''
+        Returns the current number of agents in the queue.
+        '''
 
-		return len(self.agents)
+        return len(self.agents)
 
 
 class Random_Activation(BaseScheduler):
-	'''
-	A scheduler which activates each agent once per step, in random order, 
-	with the order reshuffled every step.
+    '''
+    A scheduler which activates each agent once per step, in random order, 
+    with the order reshuffled every step.
 
-	This is equivalent to the NetLogo 'ask agents...' and is generally the 
-	default behavior for an ABM.
+    This is equivalent to the NetLogo 'ask agents...' and is generally the 
+    default behavior for an ABM.
 
-	Assumes that all agents have a step(model) method.
-	'''
+    Assumes that all agents have a step(model) method.
+    '''
 
-	def step(self):
-		'''
-		Executes the step of all agents, one at a time, in random order.
-		'''
-		random.shuffle(self.agents)
+    def step(self):
+        '''
+        Executes the step of all agents, one at a time, in random order.
+        '''
+        random.shuffle(self.agents)
 
-		for agent in self.agents:
-			agent.step(self.model)
-		self.steps += 1
-		self.time += 1
-
-
-
-
-
-
-
-		
-
-
-
-
-
+        for agent in self.agents:
+            agent.step(self.model)
+        self.steps += 1
+        self.time += 1
