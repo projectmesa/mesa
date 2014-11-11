2e44591636944bbd8ca71ad5d881f5654c6fcdbf
David Masad <david.masad@gmail.com>
2014-11-10 20:15:40 -0500

    Pylint file-specific suppressor
    Adding a pylint instruction in the space file.

--- mesa/space.py
+++ mesa/space.py
@@ -9,6 +9,9 @@ Grid: base grid, a simple list-of-lists.
 MultiGrid: extension to Grid where each cell is a set of objects.
 
 '''
+# Instruction for PyLint to suppress variable name errors, since we have a
+# good reason to use one-character variable names for x and y.
+# pylint: disable=invalid-name
 
 class Grid(object):
     '''
