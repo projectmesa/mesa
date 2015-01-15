8e25ae2a9bca52f2d8d9fcfc68fa4befe3a2a9fd
David Masad <david.masad@gmail.com>
2015-01-15 13:29:05 -0500

    Non-fixed DataCollector name for charting

--- mesa/visualization/ModularChartVisualization.py
+++ mesa/visualization/ModularChartVisualization.py
@@ -4,14 +4,13 @@ class ChartModule(VisualizationElement):
     '''
     Module for drawing live-updating line charts using Charts.js
 
-    TODO: Fix the following limitations:
-        Target model having a DataCollector named datacollector.
-        Can only use model-level variables
+    TODO: Have it be able to handle agent-level variables as well.
     '''
 
     template="chart_module.html"
 
-    def __init__(self, series, canvas_height=200, canvas_width=500):
+    def __init__(self, series, canvas_height=200, canvas_width=500,
+                 data_collector_name="datacollector"):
         '''
         Create a new line chart visualization.
 
@@ -20,6 +19,7 @@ class ChartModule(VisualizationElement):
                     HTML colors to chart them in, e.g.
                     [{"Label": "happy", "Color": "Black"},]
             canvas_height, canvas_width: Size in pixels of the chart to draw.
+            data_collector_name: Name of the DataCollector to use.
         '''
 
         self.series = series
@@ -32,12 +32,16 @@ class ChartModule(VisualizationElement):
             "series": self.series
         }
 
+        self.data_collector_name = data_collector_name
+
     def render(self, model):
         current_values = []
+        data_collector = getattr(model, self.data_collector_name)
+
         for s in self.series:
             name = s["Label"]
             try:
-                val = model.datacollector.model_vars[name][-1] # Latest value
+                val = data_collector.model_vars[name][-1] # Latest value
             except:
                 val = 0
             current_values.append(val)
