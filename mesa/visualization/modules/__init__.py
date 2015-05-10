77537db432c3ea7bc5a3d91bd12812fe1e2940d7
David Masad <david.masad@gmail.com>
2015-05-10 13:48:14 -0400

    Minor cleanup

--- mesa/visualization/modules/__init__.py
+++ mesa/visualization/modules/__init__.py
@@ -2,6 +2,6 @@
 Container for all built-in visualization modules.
 '''
 
-from mesa.visualization.modules.CanvasGridVisualization import CanvasGrid
-from mesa.visualization.modules.ChartVisualization import ChartModule
-from mesa.visualization.modules.TextVisualization import TextElement
+from mesa.visualization.modules.CanvasGridVisualization import CanvasGrid  # flake8: noqa
+from mesa.visualization.modules.ChartVisualization import ChartModule  # flake8: noqa
+from mesa.visualization.modules.TextVisualization import TextElement  # flake8: noqa
