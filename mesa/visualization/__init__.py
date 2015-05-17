6d70cd87cf52786a5d6fd78406ce7cf96c454d5c
jackiekazil <jackiekazil@gmail.com>
2015-05-17 00:42:38 -0400

    Adding settings to coverage _ updating inits to fix coverage. fixes #105.

--- mesa/visualization/__init__.py
+++ mesa/visualization/__init__.py
@@ -6,3 +6,7 @@ TextVisualization: Base class for writing ASCII visualizations of model state.
 TextServer: Class which takes a TextVisualization child class as an input, and
 renders it in-browser, along with an interface.
 '''
+
+from . import CanvasServer
+from . import TextServer
+from . import TextVisualization
