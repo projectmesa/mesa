581050860ea3882232b50cdd98f44baeceaa51fd
jackiekazil <jackiekazil@gmail.com>
2015-05-17 00:57:47 -0400

    Fixing this kind of. We need more work on code restructing.

--- mesa/__init__.py
+++ mesa/__init__.py
@@ -1,5 +1,69 @@
+'''
+Mesa Agent-Based Modeling Framework
+
+Core Objects: Agent and Model.
+'''
+
+# Import submodules
 from . import batchrunner
-from . import core
 from . import datacollection
 from . import space
 from . import time
+
+import datetime as dt
+import random
+
+
+class Agent(object):
+    '''
+    Base class for a model agent.
+    '''
+
+    model = None
+    unique_id = None
+
+    def __init__(self, unique_id, model):
+        '''
+        Create a new agent.
+        '''
+        self.model = model
+
+    def step(self, model):
+        '''
+        A single step of the agent.
+        '''
+        pass
+
+
+class Model(object):
+    '''
+    Base class for models.
+    '''
+    seed = None  # Seed for the random number generator
+    schedule = None  # Schedule object
+    running = None
+
+    def __init__(self, seed=None):
+        '''
+        Create a new model. Overload this method with the actual code to start
+        the model.
+        '''
+        if seed is None:
+            self.seed = dt.datetime.now()
+        else:
+            self.seed = seed
+        random.seed(seed)
+        self.running = True
+
+    def run_model(self):
+        '''
+        Run the model until the end condition is reached. Overload as needed.
+        '''
+        while self.running:
+            self.step()
+
+    def step(self):
+        '''
+        A single step. Fill in here.
+        '''
+        pass
