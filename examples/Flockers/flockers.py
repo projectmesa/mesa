2ef1556fa2abd40e49f3d91b32484a3bd8a76559
David Masad <david.masad@gmail.com>
2015-06-24 16:26:15 -0400

    Normalized speed

--- examples/Flockers/flockers.py
+++ examples/Flockers/flockers.py
@@ -21,16 +21,19 @@ class Boid(Agent):
         - Cohesion: steering towards neighboring agents
         - Separation:  
     '''
-    def __init__(self, unique_id, pos, velocity=None, vision=5, separation=1):
+    def __init__(self, unique_id, pos, speed=5, heading=None, 
+        vision=5, separation=1):
         '''
         Create a new Boid flocker agent.
         '''
         self.unique_id = unique_id
         self.pos = pos
-        if velocity is not None:
-            self.velocity = velocity
+        self.speed = speed
+        if heading is not None:
+            self.heading = heading
         else:
-            self.velocity
+            self.heading = np.random.random(2)
+            self.heading /= np.linalg.norm(self.heading)
         self.vision = vision
         self.separation = separation
 
@@ -53,11 +56,11 @@ class Boid(Agent):
                 sep_vector -= their_pos - my_pos
         return sep_vector
 
-    def match_velocity(self, neighbors):
-        mean_velocity = np.array([0, 0])
+    def match_heading(self, neighbors):
+        mean_heading = np.array([0, 0])
         for neighbor in neighbors:
-            mean_velocity += neighbor.velocity
-        return mean_velocity / len(neighbors)
+            mean_heading += neighbor.heading
+        return mean_heading / len(neighbors)
 
     def step(self, model):
         x, y = self.pos
@@ -65,9 +68,11 @@ class Boid(Agent):
         if len(neighbors) > 0:
             cohere_vector = self.cohere(neighbors)
             separate_vector = self.separate(neighbors)
-            match_vel_vector = self.match_velocity(neighbors)
-            self.velocity += cohere_vector + separate_vector + 0.1 * match_vel_vector
-        new_pos = np.array(self.pos) + self.velocity
+            match_heading_vector = self.match_heading(neighbors)
+            self.heading += (cohere_vector + separate_vector 
+                + match_heading_vector)
+            self.heading /= np.linalg.norm(self.heading)
+        new_pos = np.array(self.pos) + self.heading * self.speed
         new_x, new_y = new_pos
         model.space.move_agent(self, (new_x, new_y))
 
@@ -76,9 +81,10 @@ class BoidModel(Model):
     width = 100
     height = 100
 
-    def __init__(self, N, width, height, vision, separation):
+    def __init__(self, N, width, height, speed, vision, separation):
         self.N = N
         self.vision = vision
+        self.speed = speed
         self.separation = separation
         self.schedule = RandomActivation(self)
         self.space = ContinuousSpace(width, height, True,
@@ -90,9 +96,9 @@ class BoidModel(Model):
             x = random.random() * self.space.x_max
             y = random.random() * self.space.y_max
             pos = (x, y)
-            velocity = np.random.random(2)
-            velocity /= np.linalg.norm(velocity)
-            boid = Boid(i, pos, velocity, self.vision, self.separation)
+            heading = np.random.random(2)
+            heading /= np.linalg.norm(heading)
+            boid = Boid(i, pos, self.speed, heading, self.vision, self.separation)
             self.space.place_agent(boid,  pos)
             self.schedule.add(boid)
 
