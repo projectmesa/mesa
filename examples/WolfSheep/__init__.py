bcbf0c9a3192d40dfedf732a8417d5fe172bc751
David <davpaez@gmail.com>
2015-07-14 02:26:18 -0500

    Adding grass patches to WolfSheep model
    - adding parameters: grass_regrowth_time
    - reordering of arguments for WolfSheepPredation model
    - creating custom activation schedule by breed. This was done in order to emulate the procedures in the original model from netlogo
    - modifying methods for the case when grass=True
    - both wolves and sheep can die
    - they only reproduce if they are still living
    - added run_model method to show results in console, since I could get the visualization to run

--- examples/WolfSheep/__init__.py
+++ examples/WolfSheep/__init__.py
@@ -2,5 +2,6 @@ from WolfSheep import WolfSheepPredation
 from WolfSheepVisualization import WolfSheepVisualization
 
 if __name__ == "__main__":
-    model = WolfSheepPredation()
-    viz = WolfSheepVisualization(model)
+    model = WolfSheepPredation(grass=True)
+    model.run_model()
+    # viz = WolfSheepVisualization(model)
