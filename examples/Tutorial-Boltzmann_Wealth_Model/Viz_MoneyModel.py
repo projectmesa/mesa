d459f8be6637c6ecd0506f46ba67e66530ddad4b
David Masad <david.masad@gmail.com>
2015-11-04 23:07:47 -0500

    Updating all tutorial related materials.

---
+++ examples/Tutorial-Boltzmann_Wealth_Model/Viz_MoneyModel.py
@@ -0,0 +1,27 @@
+from MoneyModel import *
+from mesa.visualization.modules import CanvasGrid
+from mesa.visualization.modules import ChartModule
+from mesa.visualization.ModularVisualization import ModularServer
+
+
+def agent_portrayal(agent):
+    portrayal = {"Shape": "circle",
+                 "Filled": "true",
+                 "r": 0.5}
+
+    if agent.wealth > 0:
+        portrayal["Color"] = "red"
+        portrayal["Layer"] = 0
+    else:
+        portrayal["Color"] = "grey"
+        portrayal["Layer"] = 1
+        portrayal["r"] = 0.2
+    return portrayal
+
+grid = CanvasGrid(agent_portrayal, 10, 10, 500, 500)
+chart = ChartModule([{"Label": "Gini", "Color": "Black"}], 
+                            data_collector_name='datacollector')
+
+server = ModularServer(MoneyModel, [grid, chart], "Money Model", 100, 10, 10)
+server.port = 8889
+server.launch()
