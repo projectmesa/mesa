2213b6ff18f7ff1f44c2e2fa6ce53f5eb22edf1f
Yigit Guneli <gro.tiiy@yiit.org>
2016-07-28 19:23:02 +0300

    Adding gradient support for agents in GridVisualization
    I've renamed the Color parameter to Colors and made it an array. If the list consists of only one color then the shape is filled only with that color, otherwise it fills it with a gradient created with that colors. 
    I've also added an optional parameter called stroke_color. If it is not provided than it becomes the first given color in the colors array.

--- mesa/visualization/templates/GridDraw.js
+++ mesa/visualization/templates/GridDraw.js
@@ -21,18 +21,18 @@ time. Thus, it should be possible to turn different layers on and off.
 Here's a sample input, for a 2x2 grid with one layer being cell colors and the
 other agent locations, represented by circles:
 
-{"Shape": "rect", "x": 0, "y": 0, "Color": "#00aa00", "Filled": "true", "Layer": 0}
+{"Shape": "rect", "x": 0, "y": 0, "Colors": ["#00aa00", "#aa00aa"], "stroke_color": "red", "Filled": "true", "Layer": 0}
 
 {0:[
-        {"Shape": "rect", "x": 0, "y": 0, "w": 1, "h": 1, "Color": "#00aa00", "Filled": "true", "Layer": 0},
-	{"Shape": "rect", "x": 0, "y": 1, "w": 1, "h": 1, "Color": "#00aa00", "Filled": "true", "Layer": 0},
-	{"Shape": "rect", "x": 1, "y": 0, "w": 1, "h": 1, "Color": "#00aa00", "Filled": "true", "Layer": 0},
-	{"Shape": "rect", "x": 1, "y": 1, "w": 1, "h": 1, "Color": "#00aa00", "Filled": "true", "Layer": 0}
+        {"Shape": "rect", "x": 0, "y": 0, "w": 1, "h": 1,"Colors": ["#00aa00", "#aa00aa"], "stroke_color": "red", "Filled": "true", "Layer": 0},
+	{"Shape": "rect", "x": 0, "y": 1, "w": 1, "h": 1, "Colors": ["#00aa00", "#aa00aa"], "stroke_color": "red", "Filled": "true", "Layer": 0},
+	{"Shape": "rect", "x": 1, "y": 0, "w": 1, "h": 1, "Colors": ["#00aa00", "#aa00aa"], "stroke_color": "red", "Filled": "true", "Layer": 0},
+	{"Shape": "rect", "x": 1, "y": 1, "w": 1, "h": 1, "Colors": ["#00aa00", "#aa00aa"], "stroke_color": "red", "Filled": "true", "Layer": 0}
    ],
  1:[
-	{"Shape": "circle", "x": 0, "y": 0, "r": 0.5, "Color": "#AAAAAA", "Filled": "true", "Layer": 1, "text": 'A', "text_color": "white"},
-	{"Shape": "circle", "x": 1, "y": 1, "r": 0.5, "Color": "#AAAAAA", "Filled": "true", "Layer": 1, "text": 'B', "text_color": "white"}
-	{"Shape": "arrowHead", "x": 1, "y": 0, "heading0": -1, heading1: 0, "scale": 0.5, "Color": "green", "Filled": "true", "Layer": 1, "text": 'C', "text_color": "white"}
+	{"Shape": "circle", "x": 0, "y": 0, "r": 0.5, "Colors": ["#00aa00", "#aa00aa"], "stroke_color": "red", "Filled": "true", "Layer": 1, "text": 'A', "text_color": "white"},
+	{"Shape": "circle", "x": 1, "y": 1, "r": 0.5, "Colors": ["#00aa00", "#aa00aa"], "stroke_color": "red", "Filled": "true", "Layer": 1, "text": 'B', "text_color": "white"}
+	{"Shape": "arrowHead", "x": 1, "y": 0, "heading0": -1, heading1: 0, "scale": 0.5, "Colors": ["#00aa00", "#aa00aa"], "stroke_color": "red", "Filled": "true", "Layer": 1, "text": 'C', "text_color": "white"}
    ]
 }
 
@@ -61,12 +61,17 @@ var GridVisualization = function(width, height, gridWidth, gridHeight, context) 
                         // canvas y direction is from top to bottom. But we
                         // normally keep y-axis in plots from bottom to top.
                         p.y = gridHeight - p.y - 1;
+
+            // If the stroke color is not defined, then the first color in the colors array is the stroke color.
+            if (!p.stroke_color)
+                p.stroke_color = p.Colors[0]
+
 			if (p.Shape == "rect")
-				this.drawRectangle(p.x, p.y, p.w, p.h, p.Color, p.Filled, p.text, p.text_color);
+				this.drawRectangle(p.x, p.y, p.w, p.h, p.Colors, p.stroke_color, p.Filled, p.text, p.text_color);
 			else if (p.Shape == "circle")
-				this.drawCircle(p.x, p.y, p.r, p.Color, p.Filled, p.text, p.text_color);
-                        else if (p.Shape == "arrowHead")
-				this.drawArrowHead(p.x, p.y, p.heading0, p.heading1, p.scale, p.Color, p.Filled, p.text, p.text_color);
+				this.drawCircle(p.x, p.y, p.r, p.Colors, p.stroke_color, p.Filled, p.text, p.text_color);
+            else if (p.Shape == "arrowHead")
+				this.drawArrowHead(p.x, p.y, p.heading0, p.heading1, p.scale, p.Colors, p.stroke_color, p.Filled, p.text, p.text_color);
 		}
 	};
 
@@ -77,12 +82,13 @@ var GridVisualization = function(width, height, gridWidth, gridHeight, context) 
 	Draw a circle in the specified grid cell.
 	x, y: Grid coords
 	r: Radius, as a multiple of cell size
-	color: Code for the fill color
+	colors: List of colors for the gradient. Providing only one color will fill the shape with only that color, not gradient.
+	stroke_color: Color to stroke the shape
 	fill: Boolean for whether or not to fill the circle.
         text: Inscribed text in rectangle.
         text_color: Color of the inscribed text.
         */
-	this.drawCircle = function(x, y, radius, color, fill, text, text_color) {
+	this.drawCircle = function(x, y, radius, colors, stroke_color, fill, text, text_color) {
 		var cx = (x + 0.5) * cellWidth;
 		var cy = (y + 0.5) * cellHeight;
 		var r = radius * maxR;
@@ -91,11 +97,18 @@ var GridVisualization = function(width, height, gridWidth, gridHeight, context) 
 		context.arc(cx, cy, r, 0, Math.PI * 2, false);
 		context.closePath();
 
-		context.strokeStyle = color;
+		context.strokeStyle = stroke_color;
 		context.stroke();
 
 		if (fill) {
-			context.fillStyle = color;
+            var grd=context.createRadialGradient(cx, cy, r, cx, cy, 0);
+
+            for (i = 0; i < colors.length; i++) {
+                grd.addColorStop(i, colors[i]);
+            }
+
+            // Fill with gradient
+            context.fillStyle=grd;
 			context.fill();
 		}
 
@@ -113,12 +126,14 @@ var GridVisualization = function(width, height, gridWidth, gridHeight, context) 
 	Draw a rectangle in the specified grid cell.
 	x, y: Grid coords
 	w, h: Width and height, [0, 1]
-	color: Color for the rectangle
+	colors: List of colors for the gradient. Providing only one color will fill the shape with only that color, not gradient.
+	stroke_color: Color to stroke the shape
 	fill: Boolean, whether to fill or not.
         text: Inscribed text in rectangle.
         text_color: Color of the inscribed text.
 	*/
-	this.drawRectangle = function(x, y, w, h, color, fill, text, text_color) {
+	this.drawRectangle = function(x, y, w, h, colors, stroke_color, fill, text, text_color) {
+
 		context.beginPath();
 		var dx = w * cellWidth;
 		var dy = h * cellHeight;
@@ -127,13 +142,26 @@ var GridVisualization = function(width, height, gridWidth, gridHeight, context) 
 		var x0 = (x + 0.5) * cellWidth - dx/2;
 		var y0 = (y + 0.5) * cellHeight - dy/2;
 
-		context.strokeStyle = color;
-		context.fillStyle = color;
-		if (fill)
-			context.fillRect(x0, y0, dx, dy);
-		else
-			context.strokeRect(x0, y0, dx, dy);
+		context.strokeStyle = stroke_color;
+		context.strokeRect(x0, y0, dx, dy);
 
+		if (fill) {
+            var grd=context.createRadialGradient(x0+cellWidth/2, y0+cellHeight/2, cellHeight/2, x0+cellWidth/2, y0+cellHeight/2, 0);
+
+            for (i = 0; i < colors.length; i++) {
+                grd.addColorStop(i, colors[i]);
+            }
+
+            // Fill with gradient
+            context.fillStyle=grd;
+			context.fillRect(x0,y0,dx,dy);
+		}
+
+		else{
+			    console.log('here');
+		    context.fillStyle = color;
+			context.strokeRect(x0, y0, dx, dy);
+        }
                 // This part draws the text inside the Rectangle
                 if (text !== undefined) {
                         var cx = (x + 0.5) * cellWidth;
@@ -149,13 +177,14 @@ var GridVisualization = function(width, height, gridWidth, gridHeight, context) 
 	Draw an arrow head in the specified grid cell.
 	x, y: Grid coords
 	s: Scaling of the arrowHead with respect to cell size[0, 1]
-	color: Color for the shape
+	colors: List of colors for the gradient. Providing only one color will fill the shape with only that color, not gradient.
+	stroke_color: Color to stroke the shape
 	fill: Boolean, whether to fill or not.
         text: Inscribed text in shape.
         text_color: Color of the inscribed text.
 	*/
-        this.drawArrowHead = function(x, y, heading0, heading1, scale, color, fill, text, text_color) {
-                arrowR = maxR * scale;
+        this.drawArrowHead = function(x, y, heading0, heading1, scale, colors, stroke_color, fill, text, text_color) {
+        var arrowR = maxR * scale;
 		var cx = (x + 0.5) * cellWidth;
 		var cy = (y + 0.5) * cellHeight;
                 if (heading0 === 0 && heading1 === 1) {
@@ -206,11 +235,18 @@ var GridVisualization = function(width, height, gridWidth, gridHeight, context) 
                 context.lineTo(p4_x, p4_y);
 		context.closePath();
 
-		context.strokeStyle = color;
+		context.strokeStyle = stroke_color;
 		context.stroke();
 
 		if (fill) {
-			context.fillStyle = color;
+            var grd=context.createRadialGradient(cx, cy, arrowR*0.8, cx, cy, 0);
+
+            for (i = 0; i < colors.length; i++) {
+                grd.addColorStop(i, colors[i]);
+            }
+
+            // Fill with gradient
+            context.fillStyle=grd;
 			context.fill();
 		}
 
