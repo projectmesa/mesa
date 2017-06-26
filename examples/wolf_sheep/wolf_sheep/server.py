9b384c21f93bf31e7d9bcc5eac9b528540aaa4c3
Taylor Mutch <taylormutch@gmail.com>
2017-06-25 22:18:27 -0700

    Refactor Option to UserSettableParam.

--- examples/wolf_sheep/wolf_sheep/server.py
+++ examples/wolf_sheep/wolf_sheep/server.py
@@ -1,6 +1,6 @@
 from mesa.visualization.ModularVisualization import ModularServer
 from mesa.visualization.modules import CanvasGrid, ChartModule
-from mesa.visualization.option import Option
+from mesa.visualization.UserParam import UserSettableParameter
 
 from wolf_sheep.agents import Wolf, Sheep, GrassPatch
 from wolf_sheep.model import WolfSheepPredation
@@ -46,15 +46,15 @@ chart_element = ChartModule([{"Label": "Wolves", "Color": "#AA0000"},
 server = ModularServer(WolfSheepPredation, [canvas_element, chart_element],
                        "Wolf Sheep Predation",
                        model_params=dict(
-                           grass=Option('checkbox', 'Grass Enabled', True),
-                           grass_regrowth_time=Option('slider', 'Grass Regrowth Time', 20, 1, 50),
-                           initial_sheep=Option('slider', 'Initial Sheep Population', 100, 10, 300),
-                           sheep_reproduce=Option('slider', 'Sheep Reproduction Rate', 0.04, 0.01, 1.0, 0.01),
-                           initial_wolves=Option('slider', 'Initial Wolf Population', 50, 10, 300),
-                           wolf_reproduce=Option('slider', 'Wolf Reproduction Rate', 0.05, 0.01, 1.0, 0.01,
-                                                 description="The rate at which wolf agents reproduce."),
-                           wolf_gain_from_food=Option('slider', 'Wolf Gain From Food Rate', 20, 1, 50),
-                           sheep_gain_from_food=Option('slider', 'Sheep Gain From Food', 4, 1, 10)
+                           grass=UserSettableParameter('checkbox', 'Grass Enabled', True),
+                           grass_regrowth_time=UserSettableParameter('slider', 'Grass Regrowth Time', 20, 1, 50),
+                           initial_sheep=UserSettableParameter('slider', 'Initial Sheep Population', 100, 10, 300),
+                           sheep_reproduce=UserSettableParameter('slider', 'Sheep Reproduction Rate', 0.04, 0.01, 1.0, 0.01),
+                           initial_wolves=UserSettableParameter('slider', 'Initial Wolf Population', 50, 10, 300),
+                           wolf_reproduce=UserSettableParameter('slider', 'Wolf Reproduction Rate', 0.05, 0.01, 1.0, 0.01,
+                                                                description="The rate at which wolf agents reproduce."),
+                           wolf_gain_from_food=UserSettableParameter('slider', 'Wolf Gain From Food Rate', 20, 1, 50),
+                           sheep_gain_from_food=UserSettableParameter('slider', 'Sheep Gain From Food', 4, 1, 10)
                            )
                        )
 # server.launch()
