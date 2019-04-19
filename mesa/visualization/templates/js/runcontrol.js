ef2d022dfba9dd9a740f901387926e0dc4fdfa9c
Heß <vincent.hess@uni-graz.at>
2019-04-19 12:20:38 +0200

    Revert initGUI to original version and add small tweaks

--- mesa/visualization/templates/js/runcontrol.js
+++ mesa/visualization/templates/js/runcontrol.js
@@ -1,5 +1,3 @@
-"use strict";
-
 /* runcontrol.js
  Users can reset() the model, advance it by one step(), or start() it. reset() and
  step() send a message to the server, which then sends back the appropriate data.
@@ -33,14 +31,14 @@ function ModelController(tick = 0, fps = 3, running = false, finished = false) {
     this.finished = finished;
 
     /** Start the model and keep it running until stopped */
-    this.start = function() {
+    this.start = function start() {
         this.running = true;
         this.step();
         startModelButton.firstElementChild.innerText = "Stop";
     }
 
     /** Stop the model */
-    this.stop = function() {
+    this.stop = function stop() {
         this.running = false;
         startModelButton.firstElementChild.innerText = "Start";
     }
@@ -50,14 +48,14 @@ function ModelController(tick = 0, fps = 3, running = false, finished = false) {
      *
      * If the model is in a running state this function will be called repeatedly
      * after the visualization elements are rendered. */
-    this.step = function() {
+    this.step = function step() {
         this.tick += 1;
         stepDisplay.innerText = this.tick;
         send({ type: "get_step", step: this.tick });
     }
 
     /** Reset the model and visualization state but keep its running state */
-    this.reset = function() {
+    this.reset = function reset() {
         this.tick = 0;
         stepDisplay.innerText = this.tick;
         // Reset all the visualizations
@@ -66,22 +64,34 @@ function ModelController(tick = 0, fps = 3, running = false, finished = false) {
             this.finished = false;
             startModelButton.firstElementChild.innerText = "Start";
         }
+        clearTimeout(this.timeout)
         send({ type: "reset" });
     }
 
     /** Stops the model and put it into a finished state */
-    this.done = function() {
+    this.done = function done() {
         this.stop();
         this.finished = true;
         startModelButton.firstElementChild.innerText = "Done";
     }
 
     /**
+     * Render visualisation elements with new data.
+     * @param {any[]} data Model state data passed to the visualization elements
+     */
+    this.render = function render(data) {
+        vizElements.forEach((element, index) => element.render(data[index]))
+        if (this.running) {
+            this.timeout = setTimeout(() => this.step(), 1000 / this.fps);
+        }
+
+    /**
      * Update the frames per second
      * @param {number} val - The new value of frames per second
      */
-    this.updateFPS = function(val) {
+    this.updateFPS = function (val) {
         this.fps = Number(val);
+    }
     }
 }
 
@@ -136,13 +146,7 @@ ws.onmessage = function (message) {
     switch (msg["type"]) {
         case "viz_state":
             // Update visualization state
-            let vizData = msg["data"]
-            for (let i in vizElements) {
-                vizElements[i].render(vizData[i]);
-            }
-            if (controller.running) {
-                setTimeout(() => controller.step(), 1000 / controller.fps);
-            }
+            controller.render(msg["data"])
             break;
         case "end":
             // We have reached the end of the model
@@ -152,11 +156,6 @@ ws.onmessage = function (message) {
             // Create GUI elements for each model parameter and reset everything
             initGUI(msg["params"]);
             controller.reset();
-            break;
-        case "elements":
-            console.log("what?")
-            // Create visualization elements
-            msg["elements"].forEach(element => eval(element));
             break;
         default:
             // There shouldn't be any other message
@@ -184,203 +183,167 @@ const send = function (message) {
  * @param {object} model_params - Create the GUI from these model parameters
  */
 const initGUI = function (model_params) {
-    const addParamInput = function (param, option) {
-        switch (option["param_type"]) {
-            case "checkbox":
-                addBooleanInput(param, option);
-                break;
 
-            case "slider":
-                addSliderInput(param, option);
-                break;
+    const sidebar = $("#sidebar");
 
-            case "choice":
-                addChoiceInput(param, option);
-                break;
-
-            case "number":
-                addNumberInput(param, option); // Behaves the same as just a simple number
-                break;
-
-            case "static_text":
-                addTextBox(param, option);
-                break;
-        }
+    var onSubmitCallback = function(param_name, value) {
+        send({"type": "submit_params", "param": param_name, "value": value});
     };
 
-    for (let option in model_params) {
-        const type = typeof model_params[option];
-        const param_str = String(option);
-
-        switch (type) {
-            case "boolean":
-                addBooleanInput(param_str, {
-                    value: model_params[option],
-                    name: param_str
-                });
-                break;
-            case "number":
-                addNumberInput(param_str, {
-                    value: model_params[option],
-                    name: param_str
-                });
-                break;
-            case "object":
-                addParamInput(param_str, model_params[option]); // catch-all for params that use Option class
-                break;
-        }
-    }
-};
-
-/*
- * Input parameter definitions
- */
-
-const onSubmitCallback = function (param_name, value) {
-    send({ type: "submit_params", param: param_name, value: value });
-};
-
-const addBooleanInput = function (param, obj) {
-    var domID = param + "_id";
-    sidebar.insertAdjacentHTML(
-        "beforeend",
-        [
+    var addBooleanInput = function(param, obj) {
+        var domID = param + '_id';
+        sidebar.append([
             "<div class='input-group input-group-lg'>",
-            "<p><label for='" +
-            domID +
-            "' class='label label-primary'>" +
-            obj.name +
-            "</label></p>",
+            "<p><label for='" + domID + "' class='label label-primary'>" + obj.name + "</label></p>",
             "<input class='model-parameter' id='" + domID + "' type='checkbox'/>",
             "</div>"
-        ].join("")
-    );
-    $("#" + domID).bootstrapSwitch({
-        state: obj.value,
-        size: "small",
-        onSwitchChange: function (e, state) {
-            onSubmitCallback(param, state);
-        }
-    });
-};
+        ].join(''));
+        $('#' + domID).bootstrapSwitch({
+            'state': obj.value,
+            'size': 'small',
+            'onSwitchChange': function(e, state) {
+                onSubmitCallback(param, state);
+            }
+        });
+    };
 
-const addNumberInput = function (param, obj) {
-    var domID = param + "_id";
-    sidebar.insertAdjacentHTML(
-        "beforeend",
-        [
+    var addNumberInput = function(param, obj) {
+        var domID = param + '_id';
+        sidebar.append([
             "<div class='input-group input-group-lg'>",
-            "<p><label for='" +
-            domID +
-            "' class='label label-primary'>" +
-            obj.name +
-            "</label></p>",
+            "<p><label for='" + domID + "' class='label label-primary'>" + obj.name + "</label></p>",
             "<input class='model-parameter' id='" + domID + "' type='number'/>",
             "</div>"
-        ].join("")
-    );
-    var numberInput = $("#" + domID);
-    numberInput.val(obj.value);
-    numberInput.on("change", function () {
-        onSubmitCallback(param, Number($(this).val()));
-    });
-};
+        ].join(''));
+        var numberInput = $('#' + domID);
+        numberInput.val(obj.value);
+        numberInput.on('change', function() {
+            onSubmitCallback(param, Number($(this).val()));
+        })
+    };
 
-const addSliderInput = function (param, obj) {
-    var domID = param + "_id";
-    var tooltipID = domID + "_tooltip";
-    sidebar.insertAdjacentHTML(
-        "beforeend",
-        [
+    var addSliderInput = function(param, obj) {
+        var domID = param + '_id';
+        var tooltipID = domID + "_tooltip";
+        sidebar.append([
             "<div class='input-group input-group-lg'>",
             "<p>",
-            "<a id='" +
-            tooltipID +
-            "' data-toggle='tooltip' data-placement='top' class='label label-primary'>",
+            "<a id='" + tooltipID + "' data-toggle='tooltip' data-placement='top' class='label label-primary'>",
             obj.name,
             "</a>",
             "</p>",
             "<input id='" + domID + "' type='text' />",
             "</div>"
-        ].join("")
-    );
+        ].join(''));
 
-    // Enable tooltip label
-    if (obj.description !== null) {
-        $(tooltipID).tooltip({
-            title: obj.description,
-            placement: "right"
+        // Enable tooltip label
+        if (obj.description !== null) {
+            $(tooltipID).tooltip({
+                title: obj.description,
+                placement: 'right'
+            });
+        }
+
+        // Setup slider
+        var sliderInput = $("#" + domID);
+        sliderInput.slider({
+            min: obj.min_value,
+            max: obj.max_value,
+            value: obj.value,
+            step: obj.step,
+            ticks: [obj.min_value, obj.max_value],
+            ticks_labels: [obj.min_value, obj.max_value],
+            ticks_positions: [0, 100]
         });
-    }
+        sliderInput.on('change', function() {
+            onSubmitCallback(param, Number($(this).val()));
+        })
+    };
 
-    // Setup slider
-    var sliderInput = $("#" + domID);
-    sliderInput.slider({
-        min: obj.min_value,
-        max: obj.max_value,
-        value: obj.value,
-        step: obj.step,
-        ticks: [obj.min_value, obj.max_value],
-        ticks_labels: [obj.min_value, obj.max_value],
-        ticks_positions: [0, 100]
-    });
-    sliderInput.on("change", function () {
-        onSubmitCallback(param, Number($(this).val()));
-    });
-};
+    var addChoiceInput = function(param, obj) {
+        var domID = param + '_id';
+        var span = "<span class='caret'></span>";
+        var template = [
+            "<p><label for='" + domID + "' class='label label-primary'>" + obj.name + "</label></p>",
+            "<div class='dropdown'>",
+            "<button id='" + domID + "' class='btn btn-default dropdown-toggle' type='button' data-toggle='dropdown'>" +
+            obj.value + " " + span,
+            "</button>",
+            "<ul class='dropdown-menu' role='menu' aria-labelledby='" + domID + "'>"
+        ];
+        var choiceIdentifiers = [];
+        for (var i = 0; i < obj.choices.length; i++) {
+            var choiceID = domID + '_choice_' + i;
+            choiceIdentifiers.push(choiceID);
+            template.push(
+                "<li role='presentation'><a class='pick-choice' id='" + choiceID + "' role='menuitem' tabindex='-1' href='#'>",
+                obj.choices[i],
+                "</a></li>"
+            );
+        }
 
-const addChoiceInput = function (param, obj) {
-    var domID = param + "_id";
-    var span = "<span class='caret'></span>";
-    var template = [
-        "<p><label for='" +
-        domID +
-        "' class='label label-primary'>" +
-        obj.name +
-        "</label></p>",
-        "<div class='dropdown'>",
-        "<button id='" +
-        domID +
-        "' class='btn btn-default dropdown-toggle' type='button' data-toggle='dropdown'>" +
-        obj.value +
-        " " +
-        span,
-        "</button>",
-        "<ul class='dropdown-menu' role='menu' aria-labelledby='" + domID + "'>"
-    ];
-    var choiceIdentifiers = [];
-    for (var i = 0; i < obj.choices.length; i++) {
-        var choiceID = domID + "_choice_" + i;
-        choiceIdentifiers.push(choiceID);
-        template.push(
-            "<li role='presentation'><a class='pick-choice' id='" +
-            choiceID +
-            "' role='menuitem' tabindex='-1' href='#'>",
-            obj.choices[i],
-            "</a></li>"
-        );
-    }
+        // Close the dropdown options
+        template.push("</ul>", "</div>");
 
-    // Close the dropdown options
-    template.push("</ul>", "</div>");
-
-    // Finally render the dropdown and activate choice listeners
-    sidebar.insertAdjacentHTML("beforeend", template.join(""));
-    choiceIdentifiers.forEach(function (id, idx) {
-        $("#" + id).on("click", function () {
-            var value = obj.choices[idx];
-            $("#" + domID).html(value + " " + span);
-            onSubmitCallback(param, value);
+        // Finally render the dropdown and activate choice listeners
+        sidebar.append(template.join(''));
+        choiceIdentifiers.forEach(function (id,idx) {
+            $('#' + id).on('click', function () {
+                var value = obj.choices[idx];
+                $('#' + domID).html(value + ' ' + span);
+                onSubmitCallback(param, value);
+            });
         });
-    });
-};
+    };
 
-const addTextBox = function (param, obj) {
-    var well = $('<div class="well">' + obj.value + "</div>")[0];
-    sidebar.insertAdjacentHTML("beforeend", well);
-};
+    var addTextBox = function(param, obj) {
+        var well = $('<div class="well">' + obj.value + '</div>')[0];
+        sidebar.append(well);
+    };
 
+    var addParamInput = function(param, option) {
+        switch (option['param_type']) {
+            case 'checkbox':
+                addBooleanInput(param, option);
+                break;
+
+            case 'slider':
+                addSliderInput(param, option);
+                break;
+
+            case 'choice':
+                addChoiceInput(param, option);
+                break;
+
+            case 'number':
+                addNumberInput(param, option);   // Behaves the same as just a simple number
+                break;
+
+            case 'static_text':
+                addTextBox(param, option);
+                break;
+        }
+    };
+
+    for (var option in model_params) {
+
+        var type = typeof(model_params[option]);
+        var param_str = String(option);
+
+        switch (type) {
+            case "boolean":
+                addBooleanInput(param_str, {'value': model_params[option], 'name': param_str});
+                break;
+            case "number":
+                addNumberInput(param_str, {'value': model_params[option], 'name': param_str});
+                break;
+            case "object":
+                addParamInput(param_str, model_params[option]);    // catch-all for params that use Option class
+                break;
+        }
+    }
+};
 
 // Backward-Compatibility aliases
 const control = controller;
-const elements = vizElements
+const elements = vizElements;
