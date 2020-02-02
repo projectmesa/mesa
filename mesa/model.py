18aa8bce8e24a17c6cf6287070dae4fc2b77915d
rht <rhtbot@protonmail.com>
2020-02-02 15:58:06 -0500

    model: Add Mypy annotation

--- mesa/model.py
+++ mesa/model.py
@@ -7,12 +7,14 @@ Core Objects: Model
 """
 import time
 import random
+# mypy
+from typing import Any, Optional
 
 
 class Model:
     """ Base class for models. """
 
-    def __new__(cls, *args, **kwargs):
+    def __new__(cls, *args: Any, **kwargs: Any) -> Any:
         """Create a new model object and instantiate its RNG automatically."""
 
         model = object.__new__(cls)  # This only works in Python 3.3 and above
@@ -22,7 +24,7 @@ class Model:
         model.random = random.Random(model._seed)
         return model
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
         """ Create a new model. Overload this method with the actual code to
         start the model.
 
@@ -36,7 +38,7 @@ class Model:
         self.schedule = None
         self.current_id = 0
 
-    def run_model(self):
+    def run_model(self) -> None:
         """ Run the model until the end condition is reached. Overload as
         needed.
 
@@ -44,16 +46,16 @@ class Model:
         while self.running:
             self.step()
 
-    def step(self):
+    def step(self) -> None:
         """ A single step. Fill in here. """
         pass
 
-    def next_id(self):
+    def next_id(self) -> int:
         """ Return the next unique ID for agents, increment current_id"""
         self.current_id += 1
         return self.current_id
 
-    def reset_randomizer(self, seed=None):
+    def reset_randomizer(self, seed: Optional[int] = None) -> None:
         """Reset the model random number generator.
 
         Args:
