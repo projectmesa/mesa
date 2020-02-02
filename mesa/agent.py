c6c87c27c7e6f4fbe78817e33e203a0f59a89b86
rht <rhtbot@protonmail.com>
2020-02-02 15:52:36 -0500

    agent: Add Mypy annotation

--- mesa/agent.py
+++ mesa/agent.py
@@ -5,20 +5,27 @@ The agent class for Mesa framework.
 Core Objects: Agent
 
 """
+# mypy
+from typing import Any
+if False:
+    from .model import Model
 
 
 class Agent:
     """ Base class for a model agent. """
-    def __init__(self, unique_id, model):
+    def __init__(self, unique_id: int, model: Model) -> None:
         """ Create a new agent. """
         self.unique_id = unique_id
         self.model = model
         self.pos = None
 
-    def step(self):
+    def step(self) -> None:
         """ A single step of the agent. """
         pass
 
+    def advance(self) -> None:
+        pass
+
     @property
-    def random(self):
+    def random(self) -> Any:
         return self.model.random
