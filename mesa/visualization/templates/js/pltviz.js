2185d6b718e2d04ffa26c8286d906471aa85dd68
Vineet Verma <vineetverma.it@gmail.com>
2022-01-30 12:22:26 +0530

    update for comments on PR

--- mesa/visualization/templates/js/pltviz.js
+++ mesa/visualization/templates/js/pltviz.js
@@ -1,18 +1,17 @@
 let PyPlotModule = function(canvas_width, canvas_height) {
-    var canvas_tag = "<canvas width='" + canvas_width + "' height='" + canvas_height + "' ";
+    let canvas_tag = "<canvas width='" + canvas_width + "' height='" + canvas_height + "' ";
     canvas_tag += "style='border:1px dotted'></canvas>";
-    var canvas = $(canvas_tag)[0];
+    const canvas = $(canvas_tag)[0];
     $("#elements").append(canvas);
-    var context = canvas.getContext("2d");
-
+    const context = canvas.getContext("2d");
 
     this.render = function(data) {
-        image_data = data[1]
-        var image = new Image();
+        let image_data = data[1]
+        let image = new Image();
         image.onload = function() {
-            var hRatio = canvas.width / image.width    ;
-            var vRatio = canvas.height / image.height  ;
-            var ratio  = Math.min ( hRatio, vRatio );
+            const hRatio = canvas.width / image.width    ;
+            const vRatio = canvas.height / image.height  ;
+            const ratio  = Math.min ( hRatio, vRatio );
             context.drawImage(image, 0,0, image.width, image.height, 0,0,image.width*ratio, image.height*ratio);
         };
         image.src = "data:image/png;base64,"+image_data;
