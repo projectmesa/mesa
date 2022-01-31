c30ab5eef5ef0f62127cd4a71a9f8bc97df21678
jackiekazil <jackiekazil@gmail.com>
2022-01-31 07:37:49 -0500

    Updating version number.

--- mesa/__init__.py
+++ mesa/__init__.py
@@ -13,6 +13,6 @@ from mesa.agent import Agent
 __all__ = ["Model", "Agent"]
 
 __title__ = "mesa"
-__version__ = "0.8.9"
+__version__ = "0.9.0"
 __license__ = "Apache 2.0"
 __copyright__ = f"Copyright {datetime.date.today().year} Project Mesa Team"
