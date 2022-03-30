70fe9529f48add717fee34cf982d7a8b96a71068
rht <rhtbot@protonmail.com>
2022-03-30 01:56:48 -0400

    js: Download Bootstrap on-the-fly during install instead

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 import re
+import os
+import urllib.request
+import zipfile
+import shutil
 
 from setuptools import setup, find_packages
 from codecs import open
@@ -19,6 +23,36 @@ with open("mesa/__init__.py") as fd:
 
 with open("README.rst", "rb", encoding="utf-8") as f:
     readme = f.read()
+
+
+def merge_folders(src: str, dst: str) -> None:
+    for file_name in os.listdir(src):
+        file_path = os.path.join(src, file_name)
+        if os.path.isdir(file_path):
+            # Ensure that a folder exists, i.e. "mkdir -p"
+            dest_dir = os.path.join(dst, file_name)
+            os.makedirs(dest_dir, exist_ok=True)
+            merge_folders(file_path, dest_dir)
+            continue
+        dst_file = os.path.join(dst, file_name)
+        print("Creating", dst_file)
+        shutil.copy(file_path, dst_file)
+
+
+# Ensure Bootstrap
+if not os.path.isfile("mesa/visualization/templates/js/bootstrap.js"):
+    print("Downloading the Bootstrap dependency from the internet...")
+    url = "https://github.com/twbs/bootstrap/releases/download/v3.3.7/bootstrap-3.3.7-dist.zip"
+    zip_file = "bootstrap-dist.zip"
+    urllib.request.urlretrieve(url, zip_file)
+    with zipfile.ZipFile(zip_file, "r") as zip_ref:
+        zip_ref.extractall()
+    dir = "bootstrap-3.3.7-dist"
+    merge_folders(dir, "mesa/visualization/templates")
+    # Cleanup
+    shutil.rmtree(dir)
+    os.remove(zip_file)
+    print("Done")
 
 setup(
     name="Mesa",
