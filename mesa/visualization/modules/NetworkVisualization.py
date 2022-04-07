f1c2e681722f375882753d6f51240530e0b8606f
rht <rhtbot@protonmail.com>
2022-04-07 09:33:05 -0400

    Remove NetworkModule_sigma and its dependencies

--- mesa/visualization/modules/NetworkVisualization.py
+++ mesa/visualization/modules/NetworkVisualization.py
@@ -2,7 +2,7 @@
 Network Visualization Module
 ============
 
-Module for rendering the network, using [sigma.js](http://sigmajs.org/) or [d3.js](https://d3js.org/) frameworks.
+Module for rendering the network, using [d3.js](https://d3js.org/) framework.
 
 """
 from mesa.visualization.ModularVisualization import VisualizationElement, D3_JS_FILE
@@ -12,19 +12,12 @@ class NetworkModule(VisualizationElement):
     package_includes = []
 
     def __init__(
-        self, portrayal_method, canvas_height=500, canvas_width=500, library="sigma"
+        self,
+        portrayal_method,
+        canvas_height=500,
+        canvas_width=500,
     ):
-        library_types = ["sigma", "d3"]
-        if library not in library_types:
-            raise ValueError(
-                f"Invalid javascript library type. Expected one of: {library_types}"
-            )
-
-        NetworkModule.package_includes = (
-            ["NetworkModule_sigma.js", "sigma.min.js"]
-            if library == "sigma"
-            else ["NetworkModule_d3.js", D3_JS_FILE]
-        )
+        NetworkModule.package_includes = ["NetworkModule_d3.js", D3_JS_FILE]
 
         self.portrayal_method = portrayal_method
         self.canvas_height = canvas_height
