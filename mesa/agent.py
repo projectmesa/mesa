1e4e46feb168ecaaf1d05b89d7f46920f5fbf3f6
GaelLucero <gaelLucero.p14@gmail.com>
2022-04-08 15:20:33 -0400

    created distancexy function, which gives you the distance of the agent and the given coordinate and distance function, which gives you the distance between the agent and another agent

--- mesa/agent.py
+++ mesa/agent.py
@@ -37,9 +37,17 @@ class Agent:
     def advance(self) -> None:
         pass
 
-    def setxy(self, x,y):
+    def setxy(self, x, y):
         """Sets the current position to the specified x,y parameters"""
-        self.pos = (x,y)
+        self.pos = (x, y)
+
+    def distancexy(self, x, y):
+        """Gives you the distance of the agent and the given coordinate"""
+        return math.dist(self.pos, (x, y))
+
+    def distance(self, another_agent):
+        """Gives you the distance between the agent and another agent"""
+        return self.distancexy(another_agent.pos[0], another_agent.pos[1])
 
     @property
     def random(self) -> Random:
