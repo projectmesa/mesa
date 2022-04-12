08586793bfa06a536b17c15b5e7ff0f21a9a56de
Bryan Rojas <bryanhuepalcalco2@gmail.com>
2022-04-12 16:24:43 -0400

    removed repeated functions.

--- mesa/agent.py
+++ mesa/agent.py
@@ -86,46 +86,6 @@ class Agent:
         except:
             print("agent.py (die): could not remove agent from self.model.space")
 
-    def forward_backward(self, amount, sign):
-        """Does the calculation to find  the agent's next move and is used within the forward and backward functions"""
-        new_x = float(self.pos[0]) + sign * math.cos(self.heading * math.pi / 180) * amount
-        new_y = float(self.pos[1]) + sign * math.sin(self.heading * math.pi / -180) * amount
-        next_move = (new_x, new_y)
-        self.model.space.move_agent(self, next_move)
-
-    def forward(self, amount):
-        """Moves the agent forward by the amount given"""
-        self.forward_backward(amount, 1)
-
-    def back(self, amount):
-        """Moves the agent backwards from where its facing by the given amount"""
-        self.forward_backward(amount, -1)
-
-    def right(self, degree):
-        """Turns the agent right by the given degree"""
-        self.heading = (self.heading - degree) % 360
-
-    def left(self, degree):
-        """Turns the agent left by the given degree"""
-        self.heading = (self.heading + degree) % 360
-
-    def setxy(self, x, y):
-        """Sets the current position to the specified x,y parameters"""
-        self.pos = (x, y)
-
-    def distancexy(self, x, y):
-        """Gives you the distance of the agent and the given coordinate"""
-        return math.dist(self.pos, (x, y))
-
-    def distance(self, another_agent):
-        """Gives you the distance between the agent and another agent"""
-        return self.distancexy(another_agent.pos[0], another_agent.pos[1])
-
-    def die(self):
-        """Removes the agent from the schedule and the grid"""
-        self.model.schedule.remove(self)
-        self.model.space.remove_agent(self)
-
     def random_choice(self, adictionary):
         """Agent makes choive given dictionary values"""
         return random.choices(list(adictionary.keys()), weights=adictionary.values(), k=1)[0]
