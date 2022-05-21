182553490a8967801caff18bd17938d975129c2e
rht <rhtbot@protonmail.com>
2022-05-21 07:14:04 -0400

    feat: Implement Choice UserParam class

--- examples/pd_grid/pd_grid/server.py
+++ examples/pd_grid/pd_grid/server.py
@@ -10,8 +10,7 @@ canvas_element = mesa.visualization.CanvasGrid(portrayPDAgent, 50, 50, 500, 500)
 model_params = {
     "height": 50,
     "width": 50,
-    "schedule_type": mesa.visualization.UserSettableParameter(
-        "choice",
+    "schedule_type": mesa.visualization.Choice(
         "Scheduler type",
         value="Random",
         choices=list(PdGrid.schedule_types.keys()),
