5105ddecd29e48f4440ac3268a6522c09c34e96d
GaelLucero <gaelLucero.p14@gmail.com>
2022-05-27 17:34:47 -0400

    adding heading information and created move_forward_or_backward function, which calculates the next position the agent will move. move_forward function, which moves the agent forward by specified amount. move_backward function, which moves the agent backwards by specified amount. turn_right function, turns the agent right by specified degree. turn_left function, turns the agent left by specified degree.

--- mesa/agent.py
+++ mesa/agent.py
@@ -28,6 +28,7 @@ class Agent:
         self.unique_id = unique_id
         self.model = model
         self.pos: Optional[Position] = None
+        self.heading = 90
 
     def step(self) -> None:
         """A single step of the agent."""
@@ -35,6 +36,33 @@ class Agent:
 
     def advance(self) -> None:
         pass
+
+    def move_forward_or_backward(self, amount, sign):
+        """Does the calculation to find  the agent's next move and is used within the forward and backward functions"""
+        new_x = float(self.pos[0]) + sign * math.cos(self.heading * math.pi / 180) * amount
+        new_y = float(self.pos[1]) + sign * math.sin(self.heading * math.pi / -180) * amount
+        next_pos = (new_x, new_y)
+        try:
+            self.model.space.move_agent(self, next_pos)
+        except:
+            print("agent.py (forward_backwards): could not move agent within self.model.space")
+
+    def move_forward(self, amount):
+        """Moves the agent forward by the amount given"""
+        self.move_forward_or_backward(amount, 1)
+
+    def move_backward(self, amount):
+        """Moves the agent backwards from where its facing by the given amount"""
+        self.move_forward_or_backward(amount, -1)
+
+    def turn_right(self, degree):
+        """Turns the agent right by the given degree"""
+        self.heading = (self.heading - degree) % 360
+
+    def turn_left(self, degree):
+        """Turns the agent left by the given degree"""
+        self.heading = (self.heading + degree) % 360
+
 
     @property
     def random(self) -> Random:
