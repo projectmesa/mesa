288fe12713edf51f5b1ad010aaa5909680538869
rht <rhtbot@protonmail.com>
2022-05-28 04:50:39 -0400

    refactor: frontend: Remove jQuery usage in CanvasHexModule.js

--- mesa/visualization/templates/js/CanvasHexModule.js
+++ mesa/visualization/templates/js/CanvasHexModule.js
@@ -4,26 +4,38 @@ const CanvasHexModule = function (
   grid_width,
   grid_height
 ) {
+  const createElement = (tagName, attrs) => {
+    const element = document.createElement(tagName);
+    Object.assign(element, attrs);
+    return element;
+  };
+
   // Create the element
   // ------------------
+  const parent = createElement("div", {
+    style: `height:${canvas_height}px;`,
+    className: "world-grid-parent",
+  });
 
   // Create the tag with absolute positioning :
-  const canvas_tag = `<canvas width="${canvas_width}" height="${canvas_height}" class="world-grid"/>`;
+  const createCanvas = () => {
+    const el = createElement("canvas", {
+      width: canvas_width,
+      height: canvas_height,
+      className: "world-grid",
+    });
+    return el;
+  };
+  const canvas = createCanvas();
+  const interaction_canvas = createCanvas();
 
-  const parent_div_tag =
-    '<div style="height:' +
-    canvas_height +
-    'px;" class="world-grid-parent"></div>';
+  // Append it to parent:
+  parent.appendChild(canvas);
+  parent.appendChild(interaction_canvas);
 
-  // Append it to body:
-  const canvas = $(canvas_tag)[0];
-  const interaction_canvas = $(canvas_tag)[0];
-  const parent = $(parent_div_tag)[0];
-
-  //$("body").append(canvas);
-  $("#elements").append(parent);
-  parent.append(canvas);
-  parent.append(interaction_canvas);
+  // Append it to #elements
+  const elements = document.getElementById("elements");
+  elements.appendChild(parent);
 
   // Create the context and the drawing controller:
   const context = canvas.getContext("2d");
@@ -45,13 +57,13 @@ const CanvasHexModule = function (
     interactionHandler
   );
 
-  this.render = function (data) {
+  this.render = (data) => {
     canvasDraw.resetCanvas();
     for (const layer in data) canvasDraw.drawLayer(data[layer]);
     canvasDraw.drawGridLines("#eee");
   };
 
-  this.reset = function () {
+  this.reset = () => {
     canvasDraw.resetCanvas();
   };
 };
