2f7e871442005db14510e83ff568f0ff307c7295
rht <rhtbot@protonmail.com>
2022-05-28 05:22:19 -0400

    refactor: frontend: Remove jQuery usage in TextModule.js

--- mesa/visualization/templates/js/TextModule.js
+++ mesa/visualization/templates/js/TextModule.js
@@ -1,15 +1,15 @@
 const TextModule = function () {
-  const tag = "<p class='lead'></p>";
-  const text = $(tag)[0];
+  const text = document.createElement("p");
+  text.className = "lead";
 
   // Append text tag to #elements:
-  $("#elements").append(text);
+  document.getElementById("elements").appendChild(text);
 
   this.render = function (data) {
-    $(text).html(data);
+    text.innerHTML = data;
   };
 
   this.reset = function () {
-    $(text).html("");
+    text.innerHTML = "";
   };
 };
