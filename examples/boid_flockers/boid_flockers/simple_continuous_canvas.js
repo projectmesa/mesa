5a2212f6253388c8bf1b3de703c352e3ffee2416
rht <rhtbot@protonmail.com>
2022-06-07 08:50:08 -0400

    examples: boid_flockers: Remove usage of JS var

--- examples/boid_flockers/boid_flockers/simple_continuous_canvas.js
+++ examples/boid_flockers/boid_flockers/simple_continuous_canvas.js
@@ -1,11 +1,6 @@
-var ContinuousVisualization = function(width, height, context) {
-	var height = height;
-	var width = width;
-	var context = context;
-
+const ContinuousVisualization = function(width, height, context) {
 	this.draw = function(objects) {
-		for (var i in objects) {
-			var p = objects[i];
+		for (const p of objects) {
 			if (p.Shape == "rect")
 				this.drawRectange(p.x, p.y, p.w, p.h, p.Color, p.Filled);
 			if (p.Shape == "circle")
@@ -15,9 +10,9 @@ var ContinuousVisualization = function(width, height, context) {
 	};
 
 	this.drawCircle = function(x, y, radius, color, fill) {
-		var cx = x * width;
-		var cy = y * height;
-		var r = radius;
+		const cx = x * width;
+		const cy = y * height;
+		const r = radius;
 
 		context.beginPath();
 		context.arc(cx, cy, r, 0, Math.PI * 2, false);
@@ -35,12 +30,12 @@ var ContinuousVisualization = function(width, height, context) {
 
 	this.drawRectange = function(x, y, w, h, color, fill) {
 		context.beginPath();
-		var dx = w * width;
-		var dy = h * height;
+		const dx = w * width;
+		const dy = h * height;
 
 		// Keep the drawing centered:
-		var x0 = (x*width) - 0.5*dx;
-		var y0 = (y*height) - 0.5*dy;
+		const x0 = (x*width) - 0.5*dx;
+		const y0 = (y*height) - 0.5*dy;
 
 		context.strokeStyle = color;
 		context.fillStyle = color;
@@ -56,20 +51,26 @@ var ContinuousVisualization = function(width, height, context) {
 	};
 };
 
-var Simple_Continuous_Module = function(canvas_width, canvas_height) {
+const Simple_Continuous_Module = function(canvas_width, canvas_height) {
 	// Create the element
 	// ------------------
 
 	// Create the tag:
-	var canvas_tag = "<canvas width='" + canvas_width + "' height='" + canvas_height + "' ";
-	canvas_tag += "style='border:1px dotted'></canvas>";
+	const canvas_tag = `
+    <canvas
+      width='${canvas_width}'
+      height='${canvas_height}'
+      style='border:1px dotted'>
+    </canvas>
+  `;
+  ;
 	// Append it to body:
-	var canvas = $(canvas_tag)[0];
+	const canvas = $(canvas_tag)[0];
 	$("#elements").append(canvas);
 
 	// Create the context and the drawing controller:
-	var context = canvas.getContext("2d");
-	var canvasDraw = new ContinuousVisualization(canvas_width, canvas_height, context);
+	const context = canvas.getContext("2d");
+	const canvasDraw = new ContinuousVisualization(canvas_width, canvas_height, context);
 
 	this.render = function(data) {
 		canvasDraw.resetCanvas();
