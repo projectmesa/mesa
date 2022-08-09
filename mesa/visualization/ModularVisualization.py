3664aa25c29e71fdb2b8486df5821324e46d4338
Sascha Holzhauer <Sascha.Holzhauer@uni-kassel.de>
2022-08-09 18:40:36 +0200

    refactor ModularServer (moving code into __init__)

--- mesa/visualization/ModularVisualization.py
+++ mesa/visualization/ModularVisualization.py
@@ -250,34 +250,61 @@ class SocketHandler(tornado.websocket.WebSocketHandler):
 class ModularServer(tornado.web.Application):
     """Main visualization application."""
 
-    verbose = True
-
-    port = int(os.getenv("PORT", 8521))  # Default port to listen on
-    max_steps = 100000
-
-    # Handlers and other globals:
-    page_handler = (r"/", PageHandler)
-    socket_handler = (r"/ws", SocketHandler)
-    static_handler = (
-        r"/static/(.*)",
-        tornado.web.StaticFileHandler,
-        {"path": os.path.dirname(__file__) + "/templates"},
-    )
-    local_handler = (r"/local/(.*)", tornado.web.StaticFileHandler, {"path": ""})
-
-    handlers = [page_handler, socket_handler, static_handler, local_handler]
-
-    settings = {
-        "debug": True,
-        "autoreload": False,
-        "template_path": os.path.dirname(__file__) + "/templates",
-    }
-
     EXCLUDE_LIST = ("width", "height")
 
     def __init__(
-        self, model_cls, visualization_elements, name="Mesa Model", model_params=None
+        self,
+        model_cls,
+        visualization_elements,
+        name="Mesa Model",
+        port=None,
+        model_params=None,
     ):
+        """
+        Args:
+            model_cls: Mesa model class
+            visualization_elements: visualisation elements
+            name: A String for the model name
+            port: Port the webserver listens to (int)
+                Order of configuration:
+                1. Parameter to ModularServer.launch
+                2. Parameter to ModularServer()
+                3. Environment var PORT
+                4. Default value (8521)
+            model_params: A dict of model parameters
+        """
+
+        self.verbose = True
+        self.max_steps = 100000
+
+        if port is not None:
+            self.port = port
+        else:
+            # Default port to listen on
+            self.port = int(os.getenv("PORT", 8521))
+
+        # Handlers and other globals:
+        page_handler = (r"/", PageHandler)
+        socket_handler = (r"/ws", SocketHandler)
+        static_handler = (
+            r"/static/(.*)",
+            tornado.web.StaticFileHandler,
+            {"path": os.path.dirname(__file__) + "/templates"},
+        )
+        local_handler = (
+            r"/local/(.*)",
+            tornado.web.StaticFileHandler,
+            {"path": ""},
+        )
+
+        self.handlers = [page_handler, socket_handler, static_handler, local_handler]
+
+        self.settings = {
+            "debug": True,
+            "autoreload": False,
+            "template_path": os.path.dirname(__file__) + "/templates",
+        }
+
         """Create a new visualization server with the given elements."""
         if model_params is None:
             model_params = {}
