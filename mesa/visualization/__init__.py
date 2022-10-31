d23c4f0cac932802243157e5e6b3f23e14f250c3
Tortar <68152031+Tortar@users.noreply.github.com>
2022-10-31 20:59:02 +0100

    Update __init__.py

--- mesa/visualization/__init__.py
+++ mesa/visualization/__init__.py
@@ -6,5 +6,4 @@ TextVisualization: Base class for writing ASCII visualizations of model state.
 
 TextServer: Class which takes a TextVisualization child class as an input, and
 renders it in-browser, along with an interface.
-
 """
