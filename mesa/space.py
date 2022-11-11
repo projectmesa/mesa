6feb3e52c131fa33d6018ab94d5ec9142c97beac
Tortar <68152031+Tortar@users.noreply.github.com>
2022-11-11 11:15:22 +0100

    Update space.py

--- mesa/space.py
+++ mesa/space.py
@@ -266,7 +266,7 @@ class Grid:
             # For each dimension, in the edge case where the radius is as big as
             # possible and the dimension is even we need to shrink by one the range
             # of values to avoid duplicates in neighborhood. For example, if the
-            # width = 4 and x = radius = 2, we loop through range(0, 5) in respect 
+            # width = 4 and x = radius = 2, we loop through range(0, 5) in respect
             # to x but this means that the 0 position is repeated since 0 % 4 = 4 % 4.
             xdim_even, ydim_even = (self.width + 1) % 2, (self.height + 1) % 2
             kx = int(x_radius == x_max_radius and xdim_even)
