1b0375257bdec972c479ea75683ece877321c17f
Stephen Mubita <stephen.mubita@amfam.com>
2023-04-24 21:05:35 -0500

    Add a 'headless' option to run.py to allow wolf_sheep to run headless

--- examples/advanced/wolf_sheep/run.py
+++ examples/advanced/wolf_sheep/run.py
@@ -1,3 +1,9 @@
 from wolf_sheep.server import server
+import sys
 
-server.launch()
+open_browser: bool = True
+
+if sys.argv[1] == 'headless':
+    open_browser = False
+
+server.launch(open_browser=open_browser)
