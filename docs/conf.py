590ec2163eca6c2c9e530b046f6729e561ead603
rht <rhtbot@protonmail.com>
2023-06-17 02:37:27 -0400

    rtd: Use gruvbox-dark as style

--- docs/conf.py
+++ docs/conf.py
@@ -106,7 +106,7 @@ autoclass_content = "both"
 # show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = "sphinx"
+pygments_style = "gruvbox-dark"
 
 # A list of ignored prefixes for module index sorting.
 # modindex_common_prefix = []
