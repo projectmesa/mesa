dc38f564a614ed040fd9a3eb0308b858f82f8c34
Ewout ter Hoeven <E.M.terHoeven@student.tudelft.nl>
2023-12-17 20:07:23 +0100

    Add tests for agent removal and model agent_types property

---
+++ tests/test_agent.py
@@ -0,0 +1,16 @@
+from mesa.agent import Agent
+from mesa.model import Model
+
+
+def test_agent_removal():
+    class TestAgent(Agent):
+        pass
+
+    model = Model()
+    agent = TestAgent(model.next_id(), model)
+    # Check if the agent is added
+    assert agent in model.agents[type(agent)]
+
+    agent.remove()
+    # Check if the agent is removed
+    assert agent not in model.agents[type(agent)]
