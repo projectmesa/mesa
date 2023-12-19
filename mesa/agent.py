51d293b1fc6c168a320ee5349baac7f9e3d84d1c
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2023-12-19 16:31:18 +0100

    move to WeakKeyDictionary, add inplace boolean, do_each can return results
    _agent is now a WeakKeyDictionary, and for arguments sake, I added the inplace keyword argument

--- mesa/agent.py
+++ mesa/agent.py
@@ -7,10 +7,11 @@ Core Objects: Agent
 # Remove this __future__ import once the oldest supported Python is 3.10
 from __future__ import annotations
 
+import weakref
 from random import Random
 
 # mypy
-from typing import TYPE_CHECKING, Any, Callable, Iterator
+from typing import TYPE_CHECKING, Any, Callable, Iterator, Iterable
 
 if TYPE_CHECKING:
     # We ensure that these are not imported during runtime to prevent cyclic
@@ -63,38 +64,70 @@ class Agent:
 
 
 class AgentSet:
-    def __init__(self, agents: set[Agent], model: Model):
-        self._agents = agents
+    def __init__(self, agents: Iterable[Agent], model: Model):
+        self._agents = weakref.WeakKeyDictionary()
+
+        for agent in agents:
+            self._agents[agent] = None
+
         self.model = model
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self._agents)
 
     def __iter__(self) -> Iterator[Agent]:
-        return iter(self._agents)
+        return iter(self._agents.keys())
 
     def __contains__(self, agent: Agent) -> bool:
         """Check if an agent is in the AgentSet."""
         return agent in self._agents
 
-    def select(self, filter_func: Callable[[Agent], bool] | None = None) -> AgentSet:
+    def select(self, filter_func: Callable[[Agent], bool] | None = None, inplace: bool = False) -> AgentSet:
         if filter_func is None:
-            return AgentSet(set(self._agents), self.model)
-        return AgentSet(
-            {agent for agent in self._agents if filter_func(agent)}, self.model
-        )
+            if inplace:
+                return self
+            else:
+                return AgentSet(list(self._agents.keys()), self.model)
+        else:
+            agents = [agent for agent in self._agents.keys() if filter_func(agent)]
 
-    def shuffle(self) -> AgentSet:
-        shuffled_agents = list(self._agents)
+            if inplace:
+                self._reorder(agents)
+                return self
+            else:
+                return AgentSet(
+                    agents,
+                    self.model
+                )
+
+    def shuffle(self, inplace: bool = False) -> AgentSet:
+        shuffled_agents = list(self._agents.keys())
         self.model.random.shuffle(shuffled_agents)
-        return AgentSet(set(shuffled_agents), self.model)
 
-    def sort(self, key: Callable[[Agent], Any], reverse: bool = False) -> AgentSet:
-        sorted_agents = sorted(self._agents, key=key, reverse=reverse)
-        return AgentSet(set(sorted_agents), self.model)
+        if inplace:
+            self._reorder(shuffled_agents)
+            return self
+        else:
+            return AgentSet(shuffled_agents, self.model)
 
-    def do_each(self, method_name: str):
-        for agent in self._agents:
-            getattr(agent, method_name)()
+    def sort(self, key: Callable[[Agent], Any], reverse: bool = False, inplace: bool = False) -> AgentSet:
+        sorted_agents = sorted(list(self._agents.keys()), key=key, reverse=reverse)
+
+        if inplace:
+            self._reorder(sorted_agents)
+            return self
+        else:
+            return AgentSet(sorted_agents, self.model)
+
+    def _reorder(self, agents: Iterable[Agent]):
+        _agents = weakref.WeakKeyDictionary()
+        for agent in agents:
+            _agents[agent] = None
+        self._agents = _agents
+
+    def do_each(self, method_name: str, *args, **kwargs) -> list[Any]:
+        return [getattr(agent, method_name)(*args, **kwargs) for agent in self._agents]
+
+
 
     # Additional methods like union, intersection, difference, etc., can be added as needed.
