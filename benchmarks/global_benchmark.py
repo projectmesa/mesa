b12d6ab9bf7a068d15248a7cf0ed45af50ca6cf3
Ewout ter Hoeven <E.M.terHoeven@student.tudelft.nl>
2024-01-19 20:01:06 +0100

    test

--- benchmarks/global_benchmark.py
+++ benchmarks/global_benchmark.py
@@ -8,59 +8,64 @@ import timeit
 from configurations import configurations
 
 
-# Generic function to initialize and run a model
-def run_model(model_class, seed, parameters):
-    start_init = timeit.default_timer()
-    model = model_class(seed=seed, **parameters)
-    #   time.sleep(0.001)
+# # Generic function to initialize and run a model
+# def run_model(model_class, seed, parameters):
+#     start_init = timeit.default_timer()
+#     model = model_class(seed=seed, **parameters)
+#     #   time.sleep(0.001)
+#
+#     end_init_start_run = timeit.default_timer()
+#
+#     for _ in range(config["steps"]):
+#         model.step()
+#     #       time.sleep(0.0001)
+#     end_run = timeit.default_timer()
+#
+#     return (end_init_start_run - start_init), (end_run - end_init_start_run)
+#
+#
+# # Function to run experiments and save the fastest replication for each seed
+# def run_experiments(model_class, config):
+#     gc.enable()
+#     sys.path.insert(0, os.path.abspath("."))
+#
+#     init_times = []
+#     run_times = []
+#     for seed in range(1, config["seeds"] + 1):
+#         fastest_init = float("inf")
+#         fastest_run = float("inf")
+#         for replication in range(1, config["replications"] + 1):
+#             init_time, run_time = run_model(model_class, seed, config["parameters"])
+#             if init_time < fastest_init:
+#                 fastest_init = init_time
+#             if run_time < fastest_run:
+#                 fastest_run = run_time
+#         init_times.append(fastest_init)
+#         run_times.append(fastest_run)
+#
+#     return init_times, run_times
+#
+#
+# print(f"{time.strftime("%H:%M:%S", time.localtime())} starting benchmarks.")
+# results_dict = {}
+# for model, model_config in configurations.items():
+#     for size, config in model_config.items():
+#         results = run_experiments(model, config)
+#
+#         mean_init = sum(results[0]) / len(results[0])
+#         mean_run = sum(results[1]) / len(results[1])
+#
+#         print(f"{time.strftime("%H:%M:%S", time.localtime())} {model.__name__:<14} ({size}) timings: Init {mean_init:.5f} s; Run {mean_run:.4f} s")
+#
+#         results_dict[model, size] = results
 
-    end_init_start_run = timeit.default_timer()
-
-    for _ in range(config["steps"]):
-        model.step()
-    #       time.sleep(0.0001)
-    end_run = timeit.default_timer()
-
-    return (end_init_start_run - start_init), (end_run - end_init_start_run)
-
-
-# Function to run experiments and save the fastest replication for each seed
-def run_experiments(model_class, config):
-    gc.enable()
-    sys.path.insert(0, os.path.abspath("."))
-
-    init_times = []
-    run_times = []
-    for seed in range(1, config["seeds"] + 1):
-        fastest_init = float("inf")
-        fastest_run = float("inf")
-        for replication in range(1, config["replications"] + 1):
-            init_time, run_time = run_model(model_class, seed, config["parameters"])
-            if init_time < fastest_init:
-                fastest_init = init_time
-            if run_time < fastest_run:
-                fastest_run = run_time
-        init_times.append(fastest_init)
-        run_times.append(fastest_run)
-
-    return init_times, run_times
-
-
-print(f"{time.strftime("%H:%M:%S", time.localtime())} starting benchmarks.")
-results_dict = {}
-for model, model_config in configurations.items():
-    for size, config in model_config.items():
-        results = run_experiments(model, config)
-
-        mean_init = sum(results[0]) / len(results[0])
-        mean_run = sum(results[1]) / len(results[1])
-
-        print(f"{time.strftime("%H:%M:%S", time.localtime())} {model.__name__:<14} ({size}) timings: Init {mean_init:.5f} s; Run {mean_run:.4f} s")
-
-        results_dict[model, size] = results
+results_dict = "placeholder"
 
 # Change this name to anything you like
 save_name = "timings"
+
+# Print the contents of the current directory
+print("BEFORE: Files in the current directory:", os.listdir('.'))
 
 i = 1
 while os.path.exists(f"{save_name}_{i}.pickle"):
@@ -68,5 +73,8 @@ while os.path.exists(f"{save_name}_{i}.pickle"):
 
 with open(f"{save_name}_{i}.pickle", "wb") as handle:
     pickle.dump(results_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+# Print the contents of the current directory
+print("AFTER: Files in the current directory:", os.listdir('.'))
 
 print(f"Done benchmarking. Saved results to {save_name}_{i}.pickle.")
