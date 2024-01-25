8e7754fd6840bd19d6d1e5b7d5046e4b06c0d8dd
rht <rhtbot@protonmail.com>
2024-01-24 19:14:26 -0500

    Rename steps_ -> _steps, time_ -> _time

--- mesa/model.py
+++ mesa/model.py
@@ -70,8 +70,8 @@ class Model:
         self.current_id = 0
         self.agents_: defaultdict[type, dict] = defaultdict(dict)
 
-        self.steps_: int = 0
-        self.time_: TimeT = 0  # the model's clock
+        self._steps: int = 0
+        self._time: TimeT = 0  # the model's clock
 
         # Warning flags for current experimental features. These make sure a warning is only printed once per model.
         self.agentset_experimental_warning_given = False
@@ -119,8 +119,8 @@ class Model:
 
     def _advance_time(self, deltat: TimeT = 1):
         """Increment the model's steps counter and clock."""
-        self.steps_ += 1
-        self.time_ += deltat
+        self._steps += 1
+        self._time += deltat
 
     def next_id(self) -> int:
         """Return the next unique ID for agents, increment current_id"""
