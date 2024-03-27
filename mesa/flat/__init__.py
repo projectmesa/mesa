3c5dc401cb1deb7453b355c4249b121fc2aad33f
rht <rhtbot@protonmail.com>
2024-03-27 04:25:33 -0400

    Remove mesa.flat namespace
    This doesn't seem to be used as often as the current simple namespace.

--- mesa/flat/__init__.py
+++
@@ -1,6 +0,0 @@
-from mesa import *  # noqa
-from mesa.time import *  # noqa
-from mesa.space import *  # noqa
-from mesa.datacollection import *  # noqa
-from .visualization import *  # noqa
-from mesa.batchrunner import batch_run  # noqa
