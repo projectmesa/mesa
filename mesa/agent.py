43fa2205c6b01ef6b0138b70c4fcccd05db6f53e
Catherine Devlin <devlinch@corning.com>
2024-05-26 16:44:48 -0400

    Changes omitted from 99323b

--- mesa/agent.py
+++ mesa/agent.py
@@ -10,6 +10,7 @@ from __future__ import annotations
 
 import contextlib
 import copy
+import math
 import operator
 import warnings
 import weakref
@@ -47,7 +48,6 @@ class Agent:
         """
         self.unique_id = unique_id
         self.model = model
-<<<<<<< HEAD
         self.pos: Position | None = None
 
         # register agent
@@ -69,7 +69,7 @@ class Agent:
         """Remove and delete the agent from the model."""
         with contextlib.suppress(KeyError):
             self.model.agents_[type(self)].pop(self)
-        self.pos: Optional[Position] = None
+        self.pos: Position | None
         self.heading = 90
 
     def step(self) -> None:
@@ -78,58 +78,48 @@ class Agent:
     def advance(self) -> None:
         pass
 
+    @property
+    def random(self) -> Random:
+        return self.model.random
+
     def move_forward_or_backward(self, amount, sign):
         """Does the calculation to find  the agent's next move and is used within the forward and backward functions"""
         new_x = float(self.pos[0]) + sign * math.cos(self.heading * math.pi / 180) * amount
         new_y = float(self.pos[1]) + sign * math.sin(self.heading * math.pi / -180) * amount
         next_pos = (new_x, new_y)
+
         try:
             self.model.space.move_agent(self, next_pos)
-        except:
-            print("agent.py (forward_backwards): could not move agent within self.model.space")
+        except AttributeError as exc:
+            raise AttributeError("Agent's model does not define space") from exc
+        except Exception as exc:
+            raise UserWarning(f"agent.py (forward_backwards): could not move agent "
+                  f"{self.unique_id} within self.model.space") from exc
 
-    def move_forward(self, amount):
+    def move_forward(self, amount=1):
         """Moves the agent forward by the amount given"""
         self.move_forward_or_backward(amount, 1)
 
-    def move_backward(self, amount):
+    def move_backward(self, amount=1):
         """Moves the agent backwards from where its facing by the given amount"""
         self.move_forward_or_backward(amount, -1)
 
-    def turn_right(self, degree):
+    def turn_right(self, degree=90):
         """Turns the agent right by the given degree"""
         self.heading = (self.heading - degree) % 360
 
-    def turn_left(self, degree):
+    def turn_left(self, degree=90):
         """Turns the agent left by the given degree"""
         self.heading = (self.heading + degree) % 360
 
-    def setxy(self, x, y):
-        """Sets the current position to the specified x,y parameters"""
-        self.pos = (x, y)
-
-    def set_pos(self, apos):
-        """Sets the current position to the specified pos parameter"""
-        self.pos = apos
-
     def distancexy(self, x, y):
         """Gives you the distance of the agent and the given coordinate"""
+
         return math.dist(self.pos, (x, y))
 
     def distance(self, another_agent):
         """Gives you the distance between the agent and another agent"""
-        return self.distancexy(another_agent.pos[0], another_agent.pos[1])
-
-    def die(self):
-        """Removes the agent from the schedule and the grid """
-        try:
-            self.model.schedule.remove(self)
-        except:
-            print("agent.py (die): could not remove agent from self.model.schedule")
-        try:
-            self.model.space.remove_agent(self)
-        except:
-            print("agent.py (die): could not remove agent from self.model.space")
+        return self.distancexy(**(another_agent.pos))
 
     def towardsxy(self, x, y):
         """Calculates angle between a given coordinate and horizon as if the current position is the origin"""
@@ -152,10 +142,6 @@ class Agent:
         """Makes agent face another agent"""
         x, y = another_agent.pos
         self.facexy(x, y)
-
-    @property
-    def random(self) -> Random:
-        return self.model.random
 
 
 class AgentSet(MutableSet, Sequence):
