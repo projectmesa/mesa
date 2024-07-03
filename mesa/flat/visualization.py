b5d7d969ff3dcac93351294abab884292d222451
rht <rhtbot@protonmail.com>
2024-07-03 06:42:50 -0400

    Remove mesa.flat namespace (#2091)
    This doesn't seem to be used as often as the current simple namespace.

--- mesa/flat/visualization.py
+++
@@ -1,5 +0,0 @@
-# This collects all of Mesa visualization components under a flat namespace.
-from mesa_viz_tornado.ModularVisualization import *  # noqa
-from mesa_viz_tornado.modules import *  # noqa
-from mesa_viz_tornado.UserParam import *  # noqa
-from mesa_viz_tornado.TextVisualization import *  # noqa
