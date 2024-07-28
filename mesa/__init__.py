321f679ab4eaf0b6c5234b22303c8c317a0af03a
Aditya Tiwari <65213780+ENUMERA8OR@users.noreply.github.com>
2024-07-29 03:18:59 +0530

    Update __init__.py

--- mesa/__init__.py
+++ mesa/__init__.py
@@ -10,13 +10,6 @@ from mesa.agent import Agent
 from mesa.datacollection import DataCollector
 from mesa.model import Model
 
-
-def batch_run(*args, **kwargs):
-    from mesa.batchrunner import batch_run as _batch_run
-
-    return _batch_run(*args, **kwargs)
-
-
 __all__ = [
     "Model",
     "Agent",
@@ -32,6 +25,3 @@ __version__ = "3.0.0a0"
 __license__ = "Apache 2.0"
 _this_year = datetime.datetime.now(tz=datetime.timezone.utc).date().year
 __copyright__ = f"Copyright {_this_year} Project Mesa Team"
-
-# If 'experimental' is defined elsewhere, you might need to import it here
-# or define it if it's missing
