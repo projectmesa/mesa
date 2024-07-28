8dbb440181fad7f4fa29ce99b82057712c324342
Aditya Tiwari <65213780+ENUMERA8OR@users.noreply.github.com>
2024-07-29 03:40:34 +0530

    Update __init__.py

--- mesa/__init__.py
+++ mesa/__init__.py
@@ -2,13 +2,12 @@
 Mesa Agent-Based Modeling Framework
 Core Objects: Model, and Agent.
 """
-
 import datetime
-
 from mesa import space, time
 from mesa.agent import Agent
 from mesa.datacollection import DataCollector
 from mesa.model import Model
+from mesa.batchrunner import batch_run  # Add this line to import batch_run
 
 __all__ = [
     "Model",
@@ -16,7 +15,7 @@ __all__ = [
     "time",
     "space",
     "DataCollector",
-    "batch_run",
+    "batch_run",  # Add this line to include batch_run in __all__
     "experimental",
 ]
 
