267499ed9c72f44d9b0d5ff192a107dbe3fb303d
Ewout ter Hoeven <E.M.terHoeven@student.tudelft.nl>
2024-08-10 22:10:09 +0200

    wolf_sheep: Replace custom scheduler with AgentSet functionality
    This resolves all warnings outputted by this model.
    
    For the model step, the behavior of the old RandomActivationByType scheduler when using step(shuffle_types=True, shuffle_agents=True) is replicated. Conceptually, it can be argued that this should be modelled differently.
    
    The verbose prints are also removed.

--- examples/advanced/wolf_sheep/wolf_sheep/model.py
+++ examples/advanced/wolf_sheep/wolf_sheep/model.py
@@ -12,7 +12,6 @@ Replication of the model found in NetLogo:
 import mesa
 
 from .agents import GrassPatch, Sheep, Wolf
-from .scheduler import RandomActivationByTypeFiltered
 
 
 class WolfSheep(mesa.Model):
@@ -34,8 +33,6 @@ class WolfSheep(mesa.Model):
     grass = False
     grass_regrowth_time = 30
     sheep_gain_from_food = 4
-
-    verbose = False  # Print-monitoring
 
     description = (
         "A model for simulating wolf and sheep (predator-prey) ecosystem modelling."
@@ -81,14 +78,13 @@ class WolfSheep(mesa.Model):
         self.grass_regrowth_time = grass_regrowth_time
         self.sheep_gain_from_food = sheep_gain_from_food
 
-        self.schedule = RandomActivationByTypeFiltered(self)
         self.grid = mesa.space.MultiGrid(self.width, self.height, torus=True)
         self.datacollector = mesa.DataCollector(
             {
-                "Wolves": lambda m: m.schedule.get_type_count(Wolf),
-                "Sheep": lambda m: m.schedule.get_type_count(Sheep),
-                "Grass": lambda m: m.schedule.get_type_count(
-                    GrassPatch, lambda x: x.fully_grown
+                "Wolves": lambda m: len(m.get_agents_of_type(Wolf)),
+                "Sheep": lambda m: len(m.get_agents_of_type(Sheep)),
+                "Grass": lambda m: len(
+                    m.get_agents_of_type(GrassPatch).select(lambda a: a.fully_grown)
                 ),
             }
         )
@@ -100,7 +96,6 @@ class WolfSheep(mesa.Model):
             energy = self.random.randrange(2 * self.sheep_gain_from_food)
             sheep = Sheep(self.next_id(), self, True, energy)
             self.grid.place_agent(sheep, (x, y))
-            self.schedule.add(sheep)
 
         # Create wolves
         for i in range(self.initial_wolves):
@@ -109,7 +104,6 @@ class WolfSheep(mesa.Model):
             energy = self.random.randrange(2 * self.wolf_gain_from_food)
             wolf = Wolf(self.next_id(), self, True, energy)
             self.grid.place_agent(wolf, (x, y))
-            self.schedule.add(wolf)
 
         # Create grass patches
         if self.grass:
@@ -123,42 +117,21 @@ class WolfSheep(mesa.Model):
 
                 patch = GrassPatch(self.next_id(), self, fully_grown, countdown)
                 self.grid.place_agent(patch, (x, y))
-                self.schedule.add(patch)
 
         self.running = True
         self.datacollector.collect(self)
 
     def step(self):
-        self.schedule.step()
+        # This replicated the behavior of the old RandomActivationByType scheduler
+        # when using step(shuffle_types=True, shuffle_agents=True).
+        # Conceptually, it can be argued that this should be modelled differently.
+        self.random.shuffle(self.agent_types)
+        for agent_type in self.agent_types:
+            self.get_agents_of_type(agent_type).do("step")
+
         # collect data
         self.datacollector.collect(self)
-        if self.verbose:
-            print(
-                [
-                    self.schedule.time,
-                    self.schedule.get_type_count(Wolf),
-                    self.schedule.get_type_count(Sheep),
-                    self.schedule.get_type_count(GrassPatch, lambda x: x.fully_grown),
-                ]
-            )
 
     def run_model(self, step_count=200):
-        if self.verbose:
-            print("Initial number wolves: ", self.schedule.get_type_count(Wolf))
-            print("Initial number sheep: ", self.schedule.get_type_count(Sheep))
-            print(
-                "Initial number grass: ",
-                self.schedule.get_type_count(GrassPatch, lambda x: x.fully_grown),
-            )
-
         for i in range(step_count):
             self.step()
-
-        if self.verbose:
-            print("")
-            print("Final number wolves: ", self.schedule.get_type_count(Wolf))
-            print("Final number sheep: ", self.schedule.get_type_count(Sheep))
-            print(
-                "Final number grass: ",
-                self.schedule.get_type_count(GrassPatch, lambda x: x.fully_grown),
-            )
