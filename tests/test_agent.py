75c67d9e76db5293d3253fef63ca5ae6d8cc2a1b
Ewout ter Hoeven <E.M.terHoeven@student.tudelft.nl>
2024-08-28 19:53:34 +0200

    Add tests for select fraction

--- tests/test_agent.py
+++ tests/test_agent.py
@@ -68,6 +68,11 @@ def test_agentset():
 
     assert len(agentset.shuffle(inplace=False).select(n=5)) == 5
 
+    assert len(agentset.select(fraction=0.2)) == 2  # Select 20% of agents
+    assert len(agentset.select(fraction=0.549)) == 5  # Select 50% of agents
+    assert len(agentset.select(fraction=0)) == 0     # Select 0% of agents
+    assert len(agentset.select(fraction=1)) == 10   # Select 100% of agents
+
     def test_function(agent):
         return agent.unique_id
 
