25cdf5f5304bd986df0e6fece29ac4ae0db00cd4
Ewout ter Hoeven <E.M.terHoeven@student.tudelft.nl>
2024-09-04 10:08:29 +0200

    CI: Add script and job to test tutorials

---
+++ tests/test_tutorials.py
@@ -0,0 +1,21 @@
+import glob
+
+import pytest
+
+# Locate all notebooks in docs and subdirectories
+notebooks = glob.glob("docs/**/*.ipynb", recursive=True)
+
+
+# Parameterize the test with all found notebooks
+@pytest.mark.parametrize("notebook", notebooks)
+def test_notebook_execution(notebook):
+    # This runs each notebook using nbval and ensures there are no errors
+    pytest.main(
+        [
+            "--nbval",
+            "--current-env",  # Use the current environment
+            "--sanitize-with",
+            ".sanitize_config.yaml",  # Optional: If you want to handle variable output, use this
+            notebook,
+        ]
+    )
