3a72d95e28e0df615485c94ae5fdc2cd3a944482
vhess <hess@sec-bremen.de>
2024-09-04 13:35:13 +0200

    add new solara viz api

---
+++ mesa/visualization/__init__.py
@@ -0,0 +1,6 @@
+
+from .solara_viz import JupyterViz, Slider, SolaraViz, make_text
+from .components.altair import make_space_altair
+from .components.matplotlib import make_space_matplotlib, make_plot_measure
+
+__all__ = ["JupyterViz", "SolaraViz", "make_text", "Slider", "make_space_altair", "make_space_matplotlib", "make_plot_measure"]
