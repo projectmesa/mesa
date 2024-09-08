7cf23843ccfbfd0846517891e611704463c80961
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2024-09-08 16:58:22 +0200

    ongoing

--- mesa/experimental/signal/signal.py
+++ mesa/experimental/signal/signal.py
@@ -1,5 +1,6 @@
 import psygnal
 
+
 class Observable:
 
     def __init__(self, dtype):
@@ -17,6 +18,7 @@ class Observable:
         mt = psygnal.Signal(self.dtype)
         mt.__set_name__(type(owner), self.message_name)
         setattr(owner, self.message_name, mt)
+        setattr(owner, self.private_name, None)
 
     def __set__(self, instance, value):
         setattr(instance, self.private_name, value)
@@ -25,23 +27,57 @@ class Observable:
         a.emit(value, check_types=True)
 
 
+class MesaSignalGroup(psygnal.SignalGroup):
+    """Mesa specific subclass fo psygnal.SignalGroup"""
+
+    def __init__(self, signals, owner):
+        self._psygnal_instances = signals
+        self._psygnal_relay = psygnal._group.SignalRelay(self._psygnal_instances, owner)
+
+
+class Test:
+    signals: MesaSignalGroup
+
+    def __new__(cls, *args, **kwargs):
+        obj = super().__new__(cls)
+
+        # collect defined Signals to create a signal group
+        signals = {k: v for k, v in _defined_signals_generator(obj)}
+        setattr(obj, "signals", MesaSignalGroup(signals, obj))
+
+        return obj
+
+
+def _defined_signals_generator(obj):
+    fields = [entry for entry in dir(obj) if not entry.startswith("__")]
+    for field in fields:
+        if isinstance(instance := getattr(obj, field), psygnal.SignalInstance):
+            yield field, instance
+
+
 if __name__ == '__main__':
-    class A:
+    class A(Test):
         a = Observable(int)
 
+
+    class B(A):
+        b = Observable(int)
+
+
     a = A()
+    b = B()
 
 
-    @a.A_CHANGE.connect
-    def some_handler(*args, **kwargs):
-        print(*args, **kwargs)
-
-    def some_other_handler(arg:int):
+    def some_handler(arg: int):
         print(f"other handler {arg}")
 
 
-    a.A_CHANGE.connect(some_other_handler)
+    a.A_CHANGE.connect(some_handler)
+    b.signals.connect(some_handler)
+    b.signals.all.connect(some_handler)
+
+    print(b.signals["A_CHANGE"] == b.A_CHANGE)
 
     a.a = 5
-    a.a = 5.5
-
+    b.a = 6
+    b.b = 7
