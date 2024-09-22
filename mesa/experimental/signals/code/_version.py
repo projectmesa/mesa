5222518c6cac5722d25ba5c9576b59bc5967006a
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2024-09-22 21:40:38 +0200

    remove wrongfully commited code

--- mesa/experimental/signals/code/_version.py
+++
@@ -1,5 +0,0 @@
-from __future__ import annotations
-
-import importlib.metadata
-
-__version__ = importlib.metadata.version("signals")
--- mesa/experimental/signals/code/test.py
+++
@@ -1,9 +0,0 @@
-from _core import Computed, Signal
-
-a = Signal(2)
-b = Signal(3)
-
-c = Computed(lambda: a() + b())
-
-c()
-a.set(5)
