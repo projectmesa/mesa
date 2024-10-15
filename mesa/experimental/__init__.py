f5f781c4d2503e06c17259e2d2deb25ae608020f
Ewout ter Hoeven <E.M.terHoeven@student.tudelft.nl>
2024-10-15 08:34:01 +0200

    experimental init: Fix Solara import by making it lazy

--- mesa/experimental/__init__.py
+++ mesa/experimental/__init__.py
@@ -2,6 +2,9 @@
 
 from mesa.experimental import cell_space
 
-from .solara_viz import JupyterViz, Slider, SolaraViz, make_text
-
-__all__ = ["cell_space", "JupyterViz", "SolaraViz", "make_text", "Slider"]
+try:
+    from .solara_viz import JupyterViz, Slider, SolaraViz, make_text
+    __all__ = ["cell_space", "JupyterViz", "Slider", "SolaraViz", "make_text"]
+except ImportError:
+    print("Could not import SolaraViz. If you need it, install with 'pip install --pre mesa[viz]'")
+    __all__ = ["cell_space"]
