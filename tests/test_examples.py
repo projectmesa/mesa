f3c4add4b374e6dc38e003292f9e2951a771c177
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2024-10-19 17:15:31 +0200

    ruff related fixes

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -8,32 +8,29 @@ from mesa.examples import (
 )
 
 
-def test_boltzmann_model():
+def test_boltzmann_model():  # noqa: D103
     model = BoltzmannWealthModel(seed=42)
 
     for _i in range(10):
         model.step()
 
 
-def test_conways_game_model():
+def test_conways_game_model():  # noqa: D103
     model = ConwaysGameOfLife(seed=42)
     for _i in range(10):
         model.step()
 
-
-def test_schelling_model():
+def test_schelling_model():  # noqa: D103
     model = Schelling(seed=42)
     for _i in range(10):
         model.step()
 
-
-def test_virus_on_network():
+def test_virus_on_network():  # noqa: D103
     model = VirusOnNetwork(seed=42)
     for _i in range(10):
         model.step()
 
-
-def test_boid_flockers():
+def test_boid_flockers():  # noqa: D103
     model = BoidFlockers(seed=42)
 
     for _i in range(10):
