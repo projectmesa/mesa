3cf51d073439bbde37139ee8ad6e04cf92a894c3
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2024-10-23 20:28:26 +0200

    add solara app

--- mesa/examples/basic/conways_game_of_life/app.py
+++ mesa/examples/basic/conways_game_of_life/app.py
@@ -1,72 +1,46 @@
-import time
+import sys
+import os.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../../')))
 
-import altair as alt
-import numpy as np
-import pandas as pd
-import streamlit as st
 
 from mesa.examples.basic.conways_game_of_life.model import ConwaysGameOfLife
-
-model = st.title("Conway's Game of Life")
-num_ticks = st.slider("Select number of Steps", min_value=1, max_value=100, value=50)
-height = st.slider("Select Grid Height", min_value=10, max_value=100, step=10, value=15)
-width = st.slider("Select Grid Width", min_value=10, max_value=100, step=10, value=20)
-model = ConwaysGameOfLife(height, width)
-
-col1, col2, col3 = st.columns(3)
-status_text = st.empty()
-# step_mode = st.checkbox('Run Step-by-Step')
-run = st.button("Run Simulation")
+from mesa.visualization import (
+    SolaraViz,
+    make_plot_measure,
+    make_space_matplotlib,
+)
 
 
-if run:
-    tick = time.time()
-    step = 0
-    # init grid
-    df_grid = pd.DataFrame()
-    agent_counts = np.zeros((model.grid.width, model.grid.height))
-    for x in range(width):
-        for y in range(height):
-            df_grid = pd.concat(
-                [df_grid, pd.DataFrame({"x": [x], "y": [y], "state": [0]})],
-                ignore_index=True,
-            )
+def agent_portrayal(agent):
+    return {"color": "white" if agent.state == 0 else "black"}
 
-    heatmap = (
-        alt.Chart(df_grid)
-        .mark_point(size=100)
-        .encode(x="x", y="y", color=alt.Color("state"))
-        .interactive()
-        .properties(width=800, height=600)
-    )
 
-    # init progress bar
-    my_bar = st.progress(0, text="Simulation Progress")  # progress
-    placeholder = st.empty()
-    st.subheader("Agent Grid")
-    chart = st.altair_chart(heatmap, use_container_width=True)
-    color_scale = alt.Scale(domain=[0, 1], range=["red", "yellow"])
-    for i in range(num_ticks):
-        model.step()
-        my_bar.progress((i / num_ticks), text="Simulation progress")
-        placeholder.text("Step = %d" % i)
-        for contents, (x, y) in model.grid.coord_iter():
-            # print('x:',x,'y:',y, 'state:',contents)
-            selected_row = df_grid[(df_grid["x"] == x) & (df_grid["y"] == y)]
-            df_grid.loc[selected_row.index, "state"] = (
-                contents.state
-            )  # random.choice([1,2])
+model_params = {
+    "width": 50,
+    "height": 50,
+}
 
-        heatmap = (
-            alt.Chart(df_grid)
-            .mark_circle(size=100)
-            .encode(x="x", y="y", color=alt.Color("state", scale=color_scale))
-            .interactive()
-            .properties(width=800, height=600)
-        )
-        chart.altair_chart(heatmap)
+# Create initial model instance
+model1 = ConwaysGameOfLife(50, 50)
 
-        time.sleep(0.1)
+# Create visualization elements. The visualization elements are solara components
+# that receive the model instance as a "prop" and display it in a certain way.
+# Under the hood these are just classes that receive the model instance.
+# You can also author your own visualization elements, which can also be functions
+# that receive the model instance and return a valid solara component.
+SpaceGraph = make_space_matplotlib(agent_portrayal)
 
-    tock = time.time()
-    st.success(f"Simulation completed in {tock - tick:.2f} secs")
+
+# Create the SolaraViz page. This will automatically create a server and display the
+# visualization elements in a web browser.
+# Display it using the following command in the example directory:
+# solara run app.py
+# It will automatically update and display any changes made to this file
+page = SolaraViz(
+    model1,
+    components=[SpaceGraph],
+    model_params=model_params,
+    name="Game of Life",
+)
+page  # noqa
+
