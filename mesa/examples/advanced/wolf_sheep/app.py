eff76bbbdc2228075d56acb2d06eba472cce754d
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2024-10-24 15:57:37 +0200

    remove path trick

--- mesa/examples/advanced/wolf_sheep/app.py
+++ mesa/examples/advanced/wolf_sheep/app.py
@@ -1,9 +1,3 @@
-import sys
-import os.path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../../')))
-
-
-
 from mesa.examples.advanced.wolf_sheep.agents import Wolf, Sheep, GrassPatch
 from mesa.examples.advanced.wolf_sheep.model import WolfSheep
 from mesa.visualization import (
