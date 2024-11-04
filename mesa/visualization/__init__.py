0a3ef9fcbdf3c93b5e29ed69ebcf7e6a1cee31f6
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2024-11-04 10:54:49 +0100

    minor tweak of what's importable

--- mesa/visualization/__init__.py
+++ mesa/visualization/__init__.py
@@ -1,5 +1,7 @@
 """Solara based visualization for Mesa models."""
 
+from .components import make_plot_component, make_space_component
+from .components.altair_components import make_space_altair
 from mesa.visualization.mpl_space_drawing import (
     draw_space,
 )
@@ -14,7 +16,6 @@ __all__ = [
     "SolaraViz",
     "Slider",
     "make_space_altair",
-    "make_altair_space",
     "draw_space",
     "make_plot_component",
     "make_space_component",
