5a2887125d4f25e6c06c42a68ae84f3bfa64bb7d
Ewout ter Hoeven <E.M.terHoeven@student.tudelft.nl>
2024-11-05 16:13:06 +0100

    Mark SolaraViz as experimental for Mesa 3.0
    Add a note at four places that marks SolaraViz as experimental in Mesa 3.0

--- mesa/visualization/__init__.py
+++ mesa/visualization/__init__.py
@@ -1,4 +1,10 @@
-"""Solara based visualization for Mesa models."""
+"""Solara based visualization for Mesa models.
+
+.. note::
+    SolaraViz is experimental and still in active development for Mesa 3.0. While we attempt to minimize them, there might be API breaking changes between Mesa 3.0 and 3.1.
+
+    There won't be breaking changes between Mesa 3.0.x patch releases.
+"""
 
 from mesa.visualization.mpl_space_drawing import (
     draw_space,
