0865200918a363a7de3e3d5763443adeccf46253
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2024-11-08 13:54:40 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -175,7 +175,9 @@ html_static_path = ["_static"]
 # html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-# html_sidebars = {}
+html_sidebars = {
+    'migration_guide': [],  # No sidebar migration
+}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
