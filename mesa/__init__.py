1b4b7d3c17d9b38a5d8945677bdbf6c6afcffc9a
Ewout ter Hoeven <E.M.terHoeven@student.tudelft.nl>
2024-11-09 16:05:39 +0100

    Drop support for Python 3.10, require Python >= 3.11
    Starting development for Mesa 3.1 and following [SPEC 0](https://scientific-python.org/specs/spec-0000/) we can drop Python 3.10 support and require Python 3.11 or higher for future development. This allows adopting more modern Python features and simplifies the testing and CI configurations.
    
    See the Python 3.11 release notes: https://docs.python.org/3/whatsnew/3.11.html
    
    The 3.0.x release series will keep supporting Python 3.10.

--- mesa/__init__.py
+++ mesa/__init__.py
@@ -26,5 +26,5 @@ __all__ = [
 __title__ = "mesa"
 __version__ = "3.0.0"
 __license__ = "Apache 2.0"
-_this_year = datetime.datetime.now(tz=datetime.timezone.utc).date().year
+_this_year = datetime.datetime.now(tz=datetime.UTC).date().year
 __copyright__ = f"Copyright {_this_year} Project Mesa Team"
