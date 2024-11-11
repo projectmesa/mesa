b16a96587568abec52ea30bada985e3cb3a23bd5
Ewout ter Hoeven <E.M.terHoeven@student.tudelft.nl>
2024-11-11 14:34:41 +0100

    examples: Fix boid_flockers viz
    Boids didn't have neighbours anymore, so the visualisation crashes. This commit fixes that.

--- mesa/examples/basic/boid_flockers/app.py
+++ mesa/examples/basic/boid_flockers/app.py
@@ -3,10 +3,7 @@ from mesa.visualization import Slider, SolaraViz, make_space_component
 
 
 def boid_draw(agent):
-    if not agent.neighbors:  # Only for the first Frame
-        neighbors = len(agent.model.space.get_neighbors(agent.pos, agent.vision, False))
-    else:
-        neighbors = len(agent.neighbors)
+    neighbors = len(agent.neighbors)
 
     if neighbors <= 1:
         return {"color": "red", "size": 20}
