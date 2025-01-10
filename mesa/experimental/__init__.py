70375d1c0a0cc94b0d5de08ca01458e3bd18d06a
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2025-01-10 08:41:06 +0100

    Reimplementation of Continuous Space (#2584)

--- mesa/experimental/__init__.py
+++ mesa/experimental/__init__.py
@@ -15,6 +15,6 @@ Notes:
     - Features graduate from experimental status once their APIs are stabilized
 """
 
-from mesa.experimental import cell_space, devs, mesa_signals, meta_agents
+from mesa.experimental import cell_space, continuous_space, devs, mesa_signals, meta_agents
 
-__all__ = ["cell_space", "devs", "mesa_signals", "meta_agents"]
+__all__ = ["cell_space", "continuous_space", "devs", "mesa_signals", "meta_agents"]
