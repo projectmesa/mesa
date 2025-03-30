fb33c2498c1804b65648576e5164f5b2ec2753a3
HMNS19 <himanshur.1007@gmail.com>
2025-03-30 23:38:00 +0530

    changes focusing only on stopping model

--- mesa/visualization/solara_viz.py
+++ mesa/visualization/solara_viz.py
@@ -300,6 +300,8 @@ def ModelController(
         """Advance the model by the number of steps specified by the render_interval slider."""
         if playing.value:
             for _ in range(render_interval.value):
+                if not running.value:
+                    break
                 model.value.step()
                 running.value = model.value.running
                 if not playing.value:
@@ -311,7 +313,6 @@ def ModelController(
             for _ in range(render_interval.value):
                 if not running.value:
                     break
-                print(running.value)
                 model.value.step()
                 running.value = model.value.running
             force_update()
@@ -418,6 +419,8 @@ def SimulatorController(
         """Advance the model by the number of steps specified by the render_interval slider."""
         if playing.value:
             for _ in range(render_interval.value):
+                if not running.value:
+                    break
                 simulator.run_for(1)
                 running.value = model.value.running
                 if not playing.value:
@@ -427,6 +430,8 @@ def SimulatorController(
 
         else:
             for _ in range(render_interval.value):
+                if not running.value:
+                    break
                 simulator.run_for(1)
                 running.value = model.value.running
             force_update()
