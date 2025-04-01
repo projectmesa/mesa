8982579b1b73f3a30e8681bbdc8bb06c1d85a46c
colinfrisch <colin.frisch@gmail.com>
2025-04-02 07:49:55 +0800

    Update __init__.py

--- mesa/experimental/memory/__init__.py
+++ mesa/experimental/memory/__init__.py
@@ -4,11 +4,11 @@ This module provides the foundational data structures and classes needed for mem
 entries recording in mesa. The Memory class is a manager between the ShortTermMemory
 (dque data structure) and LongTermMemory (hash map).
 
-The module contains three main components:
-- Priority: An enumeration defining event priority levels (HIGH, DEFAULT, LOW)
-- SimulationEvent: A class representing individual events with timing and execution details
-- EventList: A heap-based priority queue managing the chronological ordering of events
 
+The module now contains four main component:
+- Memory: The operating class for managing ShortTermMemory and LongTermMemory
+- ShortTermMemory more memory-efficient and reactive (efficient store and pop functionality)
+- LongTermMemory : more computational-efficient (efficient navigation)
 """
 
 from .memory import LongTermMemory, Memory, MemoryEntry, ShortTermMemory
