f8e83f9dd14ccdffd1aee6afc960c22ee2adc507
hussainsultan <hussainz@gmail.com>
2025-05-19 12:48:41 -0400

    fix(flake): add darwin system and required overrides

--- flake.nix
+++ flake.nix
@@ -1,8 +1,10 @@
+
 {
   description = "Mesa flake using uv2nix";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
 
     pyproject-nix = {
       url = "github:pyproject-nix/pyproject.nix";
@@ -24,16 +26,21 @@
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      uv2nix,
-      pyproject-nix,
-      pyproject-build-systems,
-      ...
+    { self
+    , nixpkgs
+    , flake-utils
+    , uv2nix
+    , pyproject-nix
+    , pyproject-build-systems
+    , ...
     }:
+
+    flake-utils.lib.eachSystem ["x86_64-linux" "aarch64-darwin"] (system:
     let
       inherit (nixpkgs) lib;
+
+      # Use system-specific pkgs
+      pkgs = nixpkgs.legacyPackages.${system};
 
       # Load a uv workspace from a workspace root.
       # Uv2nix treats all uv projects as workspace projects.
@@ -41,33 +48,39 @@
 
       # Create package overlay from workspace.
       overlay = workspace.mkPyprojectOverlay {
-        # Prefer prebuilt binary wheels as a package source.
-        # Sdists are less likely to "just work" because of the metadata missing from uv.lock.
-        # Binary wheels are more likely to, but may still require overrides for library dependencies.
         sourcePreference = "wheel"; # or sourcePreference = "sdist";
-        # Optionally customise PEP 508 environment
-        # environ = {
-        #   platform_release = "5.10.65";
-        # };
       };
 
-      # Extend generated overlay with build fixups
-      #
-      # Uv2nix can only work with what it has, and uv.lock is missing essential metadata to perform some builds.
-      # This is an additional overlay implementing build fixups.
-      # See:
-      # - https://pyproject-nix.github.io/uv2nix/FAQ.html
-      pyprojectOverrides = _final: _prev: {
-        # Implement build fixups here.
-        # Note that uv2nix is _not_ using Nixpkgs buildPythonPackage.
-        # It's using https://pyproject-nix.github.io/pyproject.nix/build.html
-      };
-
-      # This example is only using x86_64-linux
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-
-      # Use Python 3.12 from nixpkgs
       python = pkgs.python312;
+
+      pyprojectOverrides = final: prev: {
+        # Add the scipy override (need to do uv add pythran)
+        scipy = prev.scipy.overrideAttrs (old: {
+          nativeBuildInputs = (old.nativeBuildInputs or []) ++ [
+            final.meson-python
+            final.ninja
+            final.cython
+            final.numpy
+            final.pybind11
+            final.packaging
+            final.pyproject-metadata
+            final.pythran
+            final.gast
+            final.beniget
+            final.ply
+          ];
+
+          buildInputs = (old.buildInputs or []) ++ [
+            pkgs.gfortran
+            pkgs.cmake
+            pkgs.xsimd
+            pkgs.pkg-config
+            pkgs.openblas
+            pkgs.meson
+            pkgs.lapack
+          ];
+        });
+      };
 
       # Construct package set
       pythonSet =
@@ -88,19 +101,19 @@
       # Package a virtual environment as our main application.
       #
       # Enable no optional dependencies for production build.
-      packages.x86_64-linux.default = pythonSet.mkVirtualEnv "mesa-env" workspace.deps.default;
+      packages.default = pythonSet.mkVirtualEnv "mesa-env" workspace.deps.default;
 
-      apps.x86_64-linux = {
+      apps = {
         default = {
           type = "app";
-          program = "${self.packages.x86_64-linux.default}/bin/ipython";
+          program = "${self.packages.${system}.default}/bin/ipython";
         };
       };
 
       # This example provides two different modes of development:
       # - Impurely using uv to manage virtual environments
       # - Pure development using uv2nix to manage virtual environments
-      devShells.x86_64-linux = {
+      devShells = {
         # It is of course perfectly OK to keep using an impure virtualenv workflow and only use uv2nix to build packages.
         # This devShell simply adds Python and undoes the dependency leakage done by Nixpkgs Python infrastructure.
         impure = pkgs.mkShell {
@@ -183,7 +196,6 @@
             # Enable all optional dependencies for development.
             virtualenv = editablePythonSet.mkVirtualEnv "mesa-dev-env" workspace.deps.all;
 
-
           in
           pkgs.mkShell {
             packages = [
@@ -217,8 +229,8 @@
               fi
             '';
           };
-        # set uv2nix devShell as default nix develop .#uv2nix
-        default = self.devShells.x86_64-linux.uv2nix;
+        # set uv2nix devShell as default
+        default = self.devShells.${system}.uv2nix;
       };
-    };
+    });
 }
