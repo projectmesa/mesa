6e6f2b42d262804625f45b6bf54d9f928f7c63c4
Sahil Chhoker <chhokersahil435@gmail.com>
2025-05-19 22:44:58 +0530

    added AgentPortrayalStyle and PropertyLayerStyle

--- mesa/visualization/components/__init__.py
+++ mesa/visualization/components/__init__.py
@@ -10,7 +10,10 @@ from .matplotlib_components import (
     make_mpl_plot_component,
     make_mpl_space_component,
 )
-
+from .portrayal_components import (
+    AgentPortrayalStyle,
+    PropertyLayerStyle,
+)
 
 def make_space_component(
     agent_portrayal: Callable | None = None,
