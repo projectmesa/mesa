359a1358aaecf867285d767d0589408487cd9abe
Sahil Chhoker <chhokersahil435@gmail.com>
2025-08-31 02:33:01 +0530

    Added multipage functionality (#2827)
    * added multipage functionality
    
    * added review changes
    
    * empty commit
    
    * fix-doc
    
    * added example, updated tutorials, added lazy loading
    
    * added reviewed changes

--- mesa/visualization/components/altair_components.py
+++ mesa/visualization/components/altair_components.py
@@ -454,6 +454,7 @@ def chart_property_layers(space, propertylayer_portrayal, chart_width, chart_hei
 def make_altair_plot_component(
     measure: str | dict[str, str] | list[str] | tuple[str],
     post_process: Callable | None = None,
+    page: int = 0,
     grid=False,
 ):
     """Create a plotting function for a specified measure.
@@ -461,16 +462,17 @@ def make_altair_plot_component(
     Args:
         measure (str | dict[str, str] | list[str] | tuple[str]): Measure(s) to plot.
         post_process: a user-specified callable to do post-processing called with the Axes instance.
+        page: Page number where the plot should be displayed.
         grid: Bool to draw grid or not.
 
     Returns:
-        function: A function that creates a PlotAltair component.
+        (function, page): A tuple of a function that creates a PlotAltair component and a page number.
     """
 
     def MakePlotAltair(model):
         return PlotAltair(model, measure, post_process=post_process, grid=grid)
 
-    return MakePlotAltair
+    return (MakePlotAltair, page)
 
 
 @solara.component
