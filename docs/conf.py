c648292b8322531d13d31c961d390f47118f26f0
Ewout ter Hoeven <15776622+EwoutH@users.noreply.github.com>
2025-11-07 12:31:50 +0100

    Update Project Mesa Team to Mesa Team

--- docs/conf.py
+++ docs/conf.py
@@ -60,7 +60,7 @@ master_doc = "index"
 
 # General information about the project.
 project = "Mesa"
-copyright = f"2015-{date.today().year}, Project Mesa Team"
+copyright = f"2015-{date.today().year}, Mesa Team"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -224,7 +224,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    ("index", "Mesa.tex", "Mesa Documentation", "Project Mesa Team", "manual")
+    ("index", "Mesa.tex", "Mesa Documentation", "Mesa Team", "manual")
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -252,7 +252,7 @@ latex_documents = [
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [("index", "mesa", "Mesa Documentation", ["Project Mesa Team"], 1)]
+man_pages = [("index", "mesa", "Mesa Documentation", ["Mesa Team"], 1)]
 
 # If true, show URL addresses after external links.
 # man_show_urls = False
@@ -268,7 +268,7 @@ texinfo_documents = [
         "index",
         "Mesa",
         "Mesa Documentation",
-        "Project Mesa Team",
+        "Mesa Team",
         "Mesa",
         "One line description of project.",
         "Miscellaneous",
