412cd5192293fd75dca59762ca46a1878d2f7381
ShreyasN <shreyasnaik81@gmail.com>
2025-12-04 22:38:36 +0530

    fixed the visualization import issue

--- tests/test_components_matplotlib.py
+++ tests/test_components_matplotlib.py
@@ -4,7 +4,6 @@ import networkx as nx
 from matplotlib.figure import Figure
 
 from mesa import Agent, Model
-from mesa.visualization import AgentPortrayalStyle
 from mesa.discrete_space import (
     CellAgent,
     HexGrid,
@@ -63,7 +62,6 @@ def test_draw_space():
             linewidths=1,
             edgecolors="tab:orange",
         )
-
 
     # draw space for hexgrid
     model = Model(seed=42)
